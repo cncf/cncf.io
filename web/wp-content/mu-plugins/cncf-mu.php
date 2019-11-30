0598a0dc421c6afa0094fbf057463b0dede2e2d1
cjyabraham <cjyabraham@gmail.com>
2019-11-30 17:41:38 +0700

    included mu plugin

---
+++ web/wp-content/mu-plugins/cncf-mu.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Core functionality for running the cncf.io site.
+ *
+ * @link              https://cncf.io/
+ * @since             1.0.0
+ * @package           cncfmu
+ *
+ * @wordpress-plugin
+ * Plugin Name:       CNCF MU
+ * Plugin URI:        https://github.com/LF-Engineering/lfevents
+ * Description:       Core functionality for running the cncf.io site.
+ * Version:           1.0.0
+ * Author:            Chris Abraham
+ * Author URI:        https://www.cncf.io
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       cncf
+ */
+
+/**
+ * Include the lfevents plugin
+ */
+require WPMU_PLUGIN_DIR . '/custom/cncf-mu/cncf-mu.php';
