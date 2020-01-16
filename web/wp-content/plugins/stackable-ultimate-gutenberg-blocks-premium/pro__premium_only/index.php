81a0a5275c98b0b8ba4b5861108f8b9746ec360b
cjyabraham <cjyabraham@gmail.com>
2020-01-16 14:37:50 +0700

    remove stackable

--- web/wp-content/plugins/stackable-ultimate-gutenberg-blocks-premium/pro__premium_only/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/**
- * Premium Block Features Initializer.
- */
-require_once( plugin_dir_path( __FILE__ ) . 'src/init.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'src/block/blog-posts/designs.php' );
