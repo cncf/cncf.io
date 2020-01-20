5a5ad85865fdf19c9a11e209910d54c6e3dfb452
James Hunt <jim@thetwopercent.co.uk>
2020-01-21 01:50:10 +0200

    Initial theme base

--- web/wp-content/themes/cncf-theme/functions.php
+++ web/wp-content/themes/cncf-theme/functions.php
@@ -5,6 +5,41 @@
  * Try to keep this file as clean as possible
  */
 
+ /**
+  * Theme Support functions
+  *
+  * Used to enable specific features of WordPress and other toolS
+  */
+if ( ! function_exists( 'the_theme_support_setup' ) ) :
+
+	function the_theme_support_setup() {
+
+		add_theme_support( 'title-tag' );
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'menus' );
+
+		add_theme_support(
+			'html5',
+			array(
+				'search-form',
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+			)
+		);
+
+		register_nav_menus(
+			array(
+				'main-menu' => esc_html__( 'Main Menu' ),
+			// 'secondary-menu' => esc_html__( 'Secondary Menu' ),
+			// 'footer-menu' => esc_html__( 'The Footer Menu' )
+			)
+		);
+
+	}
+endif;
+add_action( 'after_setup_theme', 'the_theme_support_setup' );
 
 /**
  * Image Thumbnail Sizes
@@ -30,11 +65,13 @@ $enqueue = new Enqueue();
  * Includes (enable as appropriate)
  */
 
+// theme support
+// require_once 'includes/theme-support.php';
+
 // development
 require_once 'includes/development.php';
 
-// theme support
-require_once 'includes/theme-support.php';
+
 
 // gutenberg
 require_once 'includes/gutenberg.php';
