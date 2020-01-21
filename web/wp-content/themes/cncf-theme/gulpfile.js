1bc334a90e970cea4841aab4febe4fa4085a5770
James Hunt <jim@thetwopercent.co.uk>
2020-01-21 10:41:48 +0200

    Initial theme base

--- web/wp-content/themes/cncf-theme/gulpfile.js
+++ web/wp-content/themes/cncf-theme/gulpfile.js
@@ -35,18 +35,24 @@ var globalJSWatchFiles = PROJECT_FOLDER + "/source/js/global/**/*.js";
 var projectPHPWatchFiles = PROJECT_FOLDER + "/**/**/*.php";
 var projectHTMLWatchFiles = PROJECT_FOLDER + "/**/**/*.html";
 
+var PHPTestfiles = [
+    PROJECT_FOLDER + "/**/*.php",
+    PROJECT_FOLDER + "!/vendor/**",
+    PROJECT_FOLDER + "!/node_modules/**"
+];
+
 const AUTOPREFIXER_BROWSERS = [
-	"last 2 version",
-	"> 1%",
-	"ie >= 11",
-	"ie_mob >= 10",
-	"ff >= 30",
-	"chrome >= 34",
-	"safari >= 7",
-	"opera >= 23",
-	"ios >= 7",
-	"android >= 4",
-	"bb >= 10"
+    "last 2 version",
+    "> 1%",
+    "ie >= 11",
+    "ie_mob >= 10",
+    "ff >= 30",
+    "chrome >= 34",
+    "safari >= 7",
+    "opera >= 23",
+    "ios >= 7",
+    "android >= 4",
+    "bb >= 10"
 ];
 
 /**
@@ -65,8 +71,6 @@ var mmq = require("gulp-merge-media-queries");
 /** JS plugins */
 var concat = require("gulp-concat");
 var uglify = require("gulp-uglify");
-
-// var jslint = require('gulp-jslint'); // removed
 var terser = require("gulp-terser");
 
 /** Utility plugins */
@@ -76,147 +80,148 @@ var filter = require("gulp-filter");
 var sourcemaps = require("gulp-sourcemaps");
 var del = require("del");
 var touch = require("gulp-touch-cmd");
+var phpcs = require("gulp-phpcs");
 
 var browserSync = require("browser-sync").create();
 
 function reload(done) {
-	browserSync.reload();
-	done();
+    browserSync.reload();
+    done();
 }
 
 /**
  * BrowserSync Task
  */
 function watch() {
-	gulp.watch(projectPHPWatchFiles, reload);
-	gulp.watch(projectHTMLWatchFiles).on("change", reload);
-	gulp.watch(styleWatchFiles, gulp.series([styles]));
+    gulp.watch(projectPHPWatchFiles,reload);
+    gulp.watch(projectHTMLWatchFiles).on("change",reload);
+    gulp.watch(styleWatchFiles,gulp.series([styles]));
 
-	gulp.watch(thirdpartyJSWatchFiles, gulp.series([reload]));
-	gulp.watch(blocksJSWatchFiles, gulp.series([blocksJS, reload]));
-	gulp.watch(globalJSWatchFiles, gulp.series([globalJS, reload]));
+    gulp.watch(thirdpartyJSWatchFiles,gulp.series([reload]));
+    gulp.watch(blocksJSWatchFiles,gulp.series([blocksJS,reload]));
+    gulp.watch(globalJSWatchFiles,gulp.series([globalJS,reload]));
 
-	return browserSync.init({
-		proxy: projectURL,
-		open: true,
-		notify: false
-	});
+    return browserSync.init({
+        proxy: projectURL,
+        open: true,
+        notify: false
+    });
 }
 
 /**
  * SASS to CSS tasks
  */
 function styles() {
-	return gulp
-		.src(styleSRC)
-		.pipe(sourcemaps.init())
-		.pipe(
-			sass({
-				errLogToConsole: true,
-				outputStyle: "expanded",
-				precision: 10
-			})
-		)
-		.on("error", console.error.bind(console))
-		.pipe(
-			sourcemaps.write({
-				includeContent: false
-			})
-		)
-		.pipe(
-			sourcemaps.init({
-				loadMaps: true
-			})
-		)
-		.pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
-		.pipe(sourcemaps.write())
-		.pipe(lineec())
-		.pipe(gulp.dest(styleDestination))
-		.pipe(filter("**/*.css"))
-		.pipe(
-			mmq({
-				log: true
-			})
-		)
-		.pipe(browserSync.stream())
-		.pipe(
-			rename({
-				suffix: ".min"
-			})
-		)
-		.pipe(
-			sass({
-				errLogToConsole: true,
-				outputStyle: "compressed",
-				precision: 10
-			})
-		)
-		.pipe(
-			minifycss({
-				maxLineLen: 10
-			})
-		)
-		.pipe(lineec())
-		.pipe(gulp.dest(styleDestination))
-		.pipe(filter("**/*.css"))
-		.pipe(browserSync.stream())
-		.pipe(touch());
+    return gulp
+        .src(styleSRC)
+        .pipe(sourcemaps.init())
+        .pipe(
+            sass({
+                errLogToConsole: true,
+                outputStyle: "expanded",
+                precision: 10
+            })
+        )
+        .on("error",console.error.bind(console))
+        .pipe(
+            sourcemaps.write({
+                includeContent: false
+            })
+        )
+        .pipe(
+            sourcemaps.init({
+                loadMaps: true
+            })
+        )
+        .pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
+        .pipe(sourcemaps.write())
+        .pipe(lineec())
+        .pipe(gulp.dest(styleDestination))
+        .pipe(filter("**/*.css"))
+        .pipe(
+            mmq({
+                log: true
+            })
+        )
+        .pipe(browserSync.stream())
+        .pipe(
+            rename({
+                suffix: ".min"
+            })
+        )
+        .pipe(
+            sass({
+                errLogToConsole: true,
+                outputStyle: "compressed",
+                precision: 10
+            })
+        )
+        .pipe(
+            minifycss({
+                maxLineLen: 10
+            })
+        )
+        .pipe(lineec())
+        .pipe(gulp.dest(styleDestination))
+        .pipe(filter("**/*.css"))
+        .pipe(browserSync.stream())
+        .pipe(touch());
 }
 
 /**
  * Clean build folder to help with cache
  */
 function clean() {
-	return del([styleDestination + "/*"]);
+    return del([styleDestination + "/*"]);
 }
 
 /**
  * Global JS files
  */
 function globalJS() {
-	return gulp
-		.src(jsGlobalSRC)
-		.pipe(concat(jsGlobalFile + ".js"))
-		.pipe(lineec())
-		.pipe(uglify()) // added
-		.pipe(gulp.dest(jsGlobalDestination))
-		.pipe(
-			rename({
-				basename: jsGlobalFile,
-				suffix: ".min"
-			})
-		)
-		.pipe(terser())
-		.pipe(lineec())
-		.pipe(gulp.dest(jsGlobalDestination))
-		.pipe(touch());
+    return gulp
+        .src(jsGlobalSRC)
+        .pipe(concat(jsGlobalFile + ".js"))
+        .pipe(lineec())
+        .pipe(uglify()) // added
+        .pipe(gulp.dest(jsGlobalDestination))
+        .pipe(
+            rename({
+                basename: jsGlobalFile,
+                suffix: ".min"
+            })
+        )
+        .pipe(terser())
+        .pipe(lineec())
+        .pipe(gulp.dest(jsGlobalDestination))
+        .pipe(touch());
 }
 
 /**
  * Blocks JS files
  */
 function blocksJS() {
-	return (
-		gulp
-			.src(jsBlocksSRC)
-			.pipe(concat(jsBlocksFile + ".js"))
-			.pipe(lineec())
+    return (
+        gulp
+            .src(jsBlocksSRC)
+            .pipe(concat(jsBlocksFile + ".js"))
+            .pipe(lineec())
 
-			// .pipe(uglify()) // added
-			.pipe(gulp.dest(jsBlocksDestination))
-			.pipe(
-				rename({
-					basename: jsBlocksFile,
-					suffix: ".min"
-				})
-			)
-			.pipe(terser())
-			.pipe(lineec())
-			.pipe(gulp.dest(jsBlocksDestination))
-			.pipe(touch())
-	);
+            // .pipe(uglify()) // added
+            .pipe(gulp.dest(jsBlocksDestination))
+            .pipe(
+                rename({
+                    basename: jsBlocksFile,
+                    suffix: ".min"
+                })
+            )
+            .pipe(terser())
+            .pipe(lineec())
+            .pipe(gulp.dest(jsBlocksDestination))
+            .pipe(touch())
+    );
 }
 
-exports.default = gulp.series(styles, globalJS, blocksJS, watch);
-exports.production = gulp.series(styles, globalJS, blocksJS);
+exports.default = gulp.series(styles,globalJS,blocksJS,watch);
+exports.production = gulp.series(styles,globalJS,blocksJS);
 exports.watch = gulp.series(watch);
