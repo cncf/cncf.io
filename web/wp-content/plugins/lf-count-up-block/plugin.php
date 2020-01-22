b8033b342c9e6579be73fd32df3a29b61bb5d174
fuerzastudio <contato@fuerzastudio.com.br>
2020-01-22 18:46:02 -0300

    Added custom plugin for count up block

---
+++ web/wp-content/plugins/lf-count-up-block/plugin.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: LF | Count Up Block
+ * Description: Create a custom count up Gutenberg block.
+ * Author: fuerzastudio
+ * Version: 1.0.0
+ * License: GPL2+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package CGB
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Block Initializer.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
---
+++ web/wp-content/plugins/lf-count-up-block/src/init.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * Blocks Initializer
+ *
+ * Enqueue CSS/JS of all the blocks.
+ *
+ * @since   1.0.0
+ * @package CGB
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Enqueue Gutenberg block assets for both frontend + backend.
+ *
+ * Assets enqueued:
+ * 1. blocks.style.build.css - Frontend + Backend.
+ * 2. blocks.build.js - Backend.
+ * 3. blocks.editor.build.css - Backend.
+ *
+ * @uses {wp-blocks} for block type registration & related functions.
+ * @uses {wp-element} for WP Element abstraction — structure of blocks.
+ * @uses {wp-i18n} to internationalize the block's text.
+ * @uses {wp-editor} for WP editor styles.
+ * @since 1.0.0
+ */
+function lf_count_up_block_assets() { // phpcs:ignore
+	// Register block editor script for backend.
+	wp_register_script(
+		'lf-count-up-block-js', // Handle.
+		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
+		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Dependencies, defined above.
+		filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
+		true // Enqueue the script in the footer.
+	);
+
+	wp_register_style(
+		'lf-count-up-block-style-css', // Handle.
+		plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ), // Block editor CSS.
+		array(), // Dependency to include the CSS after it.
+		filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: File modification time.
+	);
+
+	// Register block editor styles for backend.
+	wp_register_style(
+		'lf-count-up-block-editor-css', // Handle.
+		plugins_url( 'dist/blocks.editor.build.css', dirname( __FILE__ ) ), // Block editor CSS.
+		array( 'wp-edit-blocks' ), // Dependency to include the CSS after it.
+		filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: File modification time.
+	);
+
+	/**
+	 * Register Gutenberg block on server-side.
+	 *
+	 * Register the block on server-side to ensure that the block
+	 * scripts and styles for both frontend and backend are
+	 * enqueued when the editor loads.
+	 *
+	 * @link https://wordpress.org/gutenberg/handbook/blocks/writing-your-first-block-type#enqueuing-block-scripts
+	 * @since 1.16.0
+	 */
+	register_block_type(
+		'lf/count-up',
+		array(
+			'style'           => 'lf-count-up-block-style-css',
+			'editor_script'   => 'lf-count-up-block-js',
+			'editor_style'    => 'lf-count-up-block-editor-css',
+			'render_callback' => 'lf_count_up_callback',
+		)
+	);
+}
+
+function lf_count_up_callback( $attributes ) { // phpcs:ignore
+
+	$section_text = isset( $attributes['sectionText'] ) ? $attributes['sectionText'] : false;
+	$columns      = isset( $attributes['columns'] ) ? $attributes['columns'] : 1;
+
+	switch ( $columns ) {
+		case 1:
+			$column_class = 'col-sm-12';
+			break;
+		case 2:
+			$column_class = 'col-xs-12 col-sm-6';
+			break;
+		case 3:
+			$column_class = 'col-xs-12 col-sm-6 col-md-4';
+			break;
+		case 4:
+			$column_class = 'col-xxs-12 col-xs-6 col-sm-6 col-md-3';
+			break;
+	}
+
+	ob_start();
+
+	?>
+	<section class="facts-section pink-blue-section">
+		<div class="container">
+			<?php
+			if ( ! empty( $section_text ) ) :
+				echo apply_filters( 'the_content', $section_text ); // phpcs:ignore
+			endif;
+			?>
+			<div class="facts-list">
+				<div class="row">
+				<?php
+				for ( $i = 1; $i <= $columns; $i++ ) :
+					$number      = isset( $attributes[ "countUpNumber{$i}" ] ) ? $attributes[ "countUpNumber{$i}" ] : false;
+					$image       = isset( $attributes[ "icon{$i}" ] ) ? $attributes[ "icon{$i}" ] : false;
+					$description = isset( $attributes[ "descText{$i}" ] ) ? $attributes[ "descText{$i}" ] : false;
+					$link        = isset( $attributes[ "link{$i}" ] ) ? $attributes[ "link{$i}" ] : false;
+
+					if ( ! $number ) :
+						continue;
+					endif;
+					?>
+					<div class="<?php echo esc_attr( $column_class ); ?>">
+						<div class="facts-item">
+							<?php if ( ! empty( $image ) ) : ?>
+								<div class="icon-wrap">
+									<?php
+									if ( ! empty( $link ) ) :
+										printf( '<a target="_blank" href="%s">', esc_url( $link ) );
+									endif;
+									?>
+										<img src="<?php echo esc_url( $image ); ?>">
+									<?php
+									if ( ! empty( $link ) ) :
+										echo '</a>';
+									endif;
+									?>
+								</div>
+							<?php endif; ?>
+							<div class="text-wrap" data-mh="facts-text-wrap">
+								<?php
+								if ( ! empty( $link ) ) :
+									printf( '<a target="_blank" href="%s">', esc_url( $link ) );
+								endif;
+								?>
+									<div class="number" data-to="<?php echo esc_html( $number ); ?>" data-speed="4000">
+										<?php echo esc_html( $number ); ?>
+									</div>
+								<?php
+								if ( ! empty( $link ) ) :
+									echo '</a>';
+								endif;
+								?>
+								<h6>
+									<?php
+									if ( ! empty( $link ) ) :
+										printf( '<a target="_blank" href="%s">', esc_url( $link ) );
+									endif;
+										echo esc_html( $description );
+									if ( ! empty( $link ) ) :
+										echo '</a>';
+									endif;
+									?>
+								</h6>
+							</div>
+						</div>
+					</div>
+				<?php endfor; ?>
+				</div>
+			</div>
+		</div>
+	</section>
+	<?php
+
+	return ob_get_clean();
+}
+
+// Hook: Block assets.
+add_action( 'init', 'lf_count_up_block_assets' );
