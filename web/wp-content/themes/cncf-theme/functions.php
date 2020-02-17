65a01983ce27352c8d63e2b120a560d2b2242a8c
cjyabraham <cjyabraham@gmail.com>
2020-02-17 15:09:20 +0700

    fixed cache issue with post-meta-control REST end points (https://github.com/LF-Engineering/lfevents/issues/480)

--- web/wp-content/themes/cncf-theme/functions.php
+++ web/wp-content/themes/cncf-theme/functions.php
@@ -183,3 +183,28 @@ function cncf_projects_menu_filter( $items, $menu, $args ) {
 	return array_merge( $items, $child_items );
 }
 add_filter( 'wp_get_nav_menu_items', 'cncf_projects_menu_filter', 10, 3 );
+
+
+// The WP REST API is cached heavily by Pantheon so we need to explicitly exclude certain calls from the cache.
+// From https://pantheon.io/docs/mu-plugin#wp-rest-api-code-classlanguage-textwp-jsoncode-endpoints-cache.
+$regex_json_path_patterns = array(
+	'#^/wp-json/post-meta-controls/v1/?#',
+);
+foreach ( $regex_json_path_patterns as $regex_json_path_pattern ) {
+	if ( preg_match( $regex_json_path_pattern, $_SERVER['REQUEST_URI'] ) ) { //phpcs:ignore
+		// re-use the rest_post_dispatch filter in the Pantheon page cache plugin.
+		add_filter( 'rest_post_dispatch', 'filter_rest_post_dispatch_send_cache_control', 12, 2 );
+
+		/**
+		 * Re-define the send_header value with any custom Cache-Control header.
+		 *
+		 * @param obj $response Response object.
+		 * @param obj $server Server object.
+		 */
+		function filter_rest_post_dispatch_send_cache_control( $response, $server ) {
+			$server->send_header( 'Cache-Control', 'no-cache, must-revalidate, max-age=0' );
+			return $response;
+		}
+		break;
+	}
+}
