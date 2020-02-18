f29d954ae139947a2f80192540d256ac586c52b7
James Hunt <jim@thetwopercent.co.uk>
2020-02-18 17:50:55 +0200

    Fix code sniffs

--- web/wp-content/themes/cncf-theme/includes/gutenberg-block-styles.php
+++ web/wp-content/themes/cncf-theme/includes/gutenberg-block-styles.php
@@ -9,3 +9,29 @@
  * @since 1.0.0
  */
 
+/**
+ * Block Template basic setup
+ */
+function slug_post_type_template() {
+	$page_type_object           = get_post_type_object( 'page' );
+	$page_type_object->template = array(
+		array(
+			'core/heading',
+			array(
+				'level'   => '1',
+				'content' => 'Title of page',
+				'className' => 'is-style-max-900',
+			),
+		),
+		array( 'core/paragraph' ),
+		array(
+			'core/heading',
+			array(
+				'level'   => '2',
+				'content' => 'Sub header',
+			),
+		),
+		array( 'core/paragraph' ),
+	);
+};
+add_action( 'init', 'slug_post_type_template' );
