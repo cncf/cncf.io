6d8882f50cec94620427910ca87e0efd0c791715
James Hunt <jim@thetwopercent.co.uk>
2020-02-18 10:54:39 +0200

    Block styles

--- web/wp-content/themes/cncf-theme/includes/gutenberg-block-styles.php
+++ web/wp-content/themes/cncf-theme/includes/gutenberg-block-styles.php
@@ -9,28 +9,3 @@
  * @since 1.0.0
  */
 
-register_block_style(
-	'core/heading',
-	array(
-		'name'         => 'uppercase-heading',
-		'label'        => 'Uppercase',
-		'inline_style' => '.is-style-uppercase-heading { text-transform: uppercase; }',
-	)
-);
-
-register_block_style(
-	'core/paragraph',
-	array(
-		'name'  => 'blue-paragraph',
-		'label' => 'Blue Paragraph',
-	)
-);
-
-register_block_style(
-	'core/quote',
-	array(
-		'name'         => 'blue-quote',
-		'label'        => __( 'Blue Quote' ),
-		'inline_style' => '.wp-block-quote.is-style-blue-quote { color: blue; }',
-	)
-);
