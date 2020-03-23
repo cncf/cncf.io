8f6f182ffb8a6574bc974666508f9ef2d8c88828
cjyabraham <cjyabraham@gmail.com>
2020-03-23 19:46:32 +0800

    adding um emails

---
+++ web/wp-content/themes/cncf-theme/ultimate-member/email/pending_email.php
@@ -0,0 +1,14 @@
+<div style="max-width: 560px; padding: 20px; background: #ffffff; border-radius: 5px; margin: 40px auto; font-family: Open Sans,Helvetica,Arial; font-size: 15px; color: #666;">
+<div style="color: #444444; font-weight: normal;">
+<div style="text-align: center; font-weight: 600; font-size: 26px; padding: 10px 0; border-bottom: solid 3px #eeeeee;">{site_name}</div>
+<div style="clear: both;"> </div>
+</div>
+<div style="padding: 0 30px 30px 30px; border-bottom: 3px solid #eeeeee;">
+<div style="padding: 30px 0; font-size: 24px; text-align: center; line-height: 40px;">Thank you for signing up!<span style="display: block;">Your account has to be manually reviewed.</span></div>
+<div style="padding: 15px; background: #eee; border-radius: 3px; text-align: center;">Speakers Bureau requests are reviewed weekly. You will receive an email accepting or rejecting your application.</div>
+</div>
+<div style="color: #999; padding: 20px 30px;">
+<div>Thank you!</div>
+<div>The <a style="color: #3ba1da; text-decoration: none;" href="{site_url}">{site_name}</a> Team</div>
+</div>
+</div>
