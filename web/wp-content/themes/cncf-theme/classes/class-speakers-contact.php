259eced7343855e5fc1b41df1746767e2dc1f421
cjyabraham <cjyabraham@gmail.com>
2020-03-26 14:55:01 +0800

    for some reason this line was tripping the circleci code sniff

--- web/wp-content/themes/cncf-theme/classes/class-speakers-contact.php
+++ web/wp-content/themes/cncf-theme/classes/class-speakers-contact.php
@@ -212,7 +212,7 @@ class Speakers_Contact {
 				return str_replace( '"', '', $item );
 			},
 			$speakers,
-		);
+		); //phpcs:ignore
 
 		$params = compact( 'name', 'email', 'start_date', 'end_date', 'subject', 'message', 'language', 'travel_funding', 'attendees_number' );
 
