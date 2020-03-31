6d55214fa724cc8bda7e1118879c381316dc3adc
James Hunt <jim@thetwopercent.co.uk>
2020-03-31 20:43:27 +0100

    Adding quick Twitter feed block

---
+++ web/wp-content/plugins/lf-cncf-blocks/src/twitter-feed/block.js
@@ -0,0 +1,62 @@
+/**
+ * Register block JS
+ *
+ * @package WordPress
+ * @since 1.0.0
+ *
+ * @tags
+ * @phpcs:disable WordPress.WhiteSpace.OperatorSpacing.NoSpaceAfter
+ * @phpcs:disable WordPress.WhiteSpace.OperatorSpacing.NoSpaceBefore
+ * @phpcs:disable Generic.WhiteSpace.ScopeIndent.IncorrectExact
+ * @phpcs:disable Generic.WhiteSpace.ScopeIndent.Incorrect
+ * @phpcs:disable PEAR.Functions.FunctionCallSignature.Indent
+ */
+
+// Import CSS.
+import './editor.scss';
+import './style.scss';
+
+import Edit from './edit.js';
+
+const { __ } = wp.i18n;
+const { registerBlockType } = wp.blocks;
+
+/**
+ * Register: aa Gutenberg Block.
+ *
+ * Registers a new block provided a unique name and an object defining its
+ * behavior. Once registered, the block is made editor as an option to any
+ * editor interface where blocks are implemented.
+ *
+ * @link https://wordpress.org/gutenberg/handbook/block-api/
+ * @param  {string}   name     Block name.
+ * @param  {Object}   settings Block settings.
+ * @return {?WPBlock}          The block, if it has been successfully registered; otherwise `undefined`.
+ */
+registerBlockType(
+	 'lf/twitter-feed',
+	{
+		title: __( 'LF | Twitter Feed' ),
+		description: __( 'Showing the latest tweets from CNCF.io' ),
+		icon: 'twitter',
+		category: 'cncf',
+		keywords: [
+			__( 'twitter' ),
+			__( 'feed' ),
+			__( 'social' ),
+			__( 'cncf' ),
+		],
+		example: {},
+		attributes: {
+			numberposts: {
+				type: 'integer',
+				default: 4,
+			},
+		},
+		html: false,
+		edit: Edit,
+		save() {
+			return null;
+		},
+	}
+);
