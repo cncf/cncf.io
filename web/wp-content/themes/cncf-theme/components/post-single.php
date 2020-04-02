3c6c659f9add8348dec4deef6ccdbd734d62c1f8
James Hunt <jim@thetwopercent.co.uk>
2020-04-02 17:15:03 +0100

    Single post improvements

--- web/wp-content/themes/cncf-theme/components/post-single.php
+++ web/wp-content/themes/cncf-theme/components/post-single.php
@@ -11,16 +11,22 @@
 <section class="hero">
 	<div class="container wrap">
 
-	<?php
-	// Category of the post.
-	foreach ( get_the_category() as $category ) {
-		echo '<p class="post-single-category">';
-		if ( $category->name ) :
-			echo '<a href="' . get_category_link( $category->term_id ) . '" title="See Posts in ' . $category->name . '">' . $category->name . '</a>';
-		endif;
-		echo '</p>';
-	}
-	?>
+		<?php
+		// Category of the post.
+		$all_categories = get_the_category();
+		if ( $all_categories ) :
+			?>
+<p class="post-single-category">
+			<?php
+			// Only get the first item in the array.
+			$category = array_shift( $all_categories );
+			// $category = $the_category->name;
+			echo '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" title="See Posts in ' . esc_html( $category->name ) . '">' . esc_html( $category->name ) . '</a>';
+			?>
+	</p>
+			<?php
+endif;
+		?>
 
 		<h1 class="post-title" itemprop="headline"><?php the_title(); ?></h1>
 	</div>
@@ -31,30 +37,50 @@
 		while ( have_posts() ) :
 			the_post();
 			?>
-		<p class="post-single-meta">Posted on <?php the_date(); ?> <?php echo 'by ' . get_the_author(); ?></p>
-			<?php
-			// Get the terms associated.
-			$term_obj_list   = get_the_terms( $post->ID, 'cncf-author-category' );
-			$category_author = join( ', ', wp_list_pluck( $term_obj_list, 'name' ) );
+
+<?php
+			// Get the Category Author terms associated if any.
+			$terms = get_the_terms( $post->ID, 'cncf-author-category' );
+			if ( $terms ) {
+				// Only get the first item in the array.
+				$term            = array_shift( $terms );
+				$category_author = $term->name;
+			}
 			if ( ! empty( $category_author ) ) :
 				?>
-			<p class="post-single-author-category"><?php echo $category_author; ?></p>
+		<p class="post-single-author-category ">CNCF <?php echo $category_author; ?> Blog Post</p>
 				<?php
 			endif;
 			?>
 
+			<?php
+			// TODO: substitute this block name for the Guest Post block.
+			// if ( has_block( 'guest-byline' ) : //phpcs:ignore.
+			if ( ! has_block( 'core/headings' ) ) :
+				?>
+		<p class="post-single-meta">Posted on <?php the_date(); ?></p>
+		<p class="post-single-meta"><?php echo 'By ' . get_the_author(); ?></p>
+				<?php
+endif;
+			?>
+
 
 		<div class="entry-content">
-			<?php the_content(); ?>
 
 			<?php
+
+			the_content();
+
 			get_template_part( 'components/social-share' );
+
 			?>
 
 			<hr class="post-single-hr">
+
 			<?php
 			get_template_part( 'components/post-pagination' );
 			?>
+
 		</div>
 			<?php
 endwhile;
