c78a5f8e4f69d1de7ceafda0a789fb12940ec372
James Hunt <jim@thetwopercent.co.uk>
2020-04-10 13:57:42 +0100

    Adding page templates directory

---
+++ web/wp-content/themes/cncf-theme/templates/template-webinars.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Webinars
+ *
+ * @package WordPress
+ * @subpackage cncf-theme
+ * @since 1.0.0
+ */
+
+get_template_part( 'components/header' );
+
+get_template_part( 'components/hero' );
+
+get_template_part( 'components/webinar-layout' );
+
+get_template_part( 'components/footer' );
