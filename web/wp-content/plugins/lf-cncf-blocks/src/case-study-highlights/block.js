9056b7f93b1eb7ea678c1a92b6e3cdaddffec88b
James Hunt <jim@thetwopercent.co.uk>
2020-05-05 19:16:02 +0100

    New gutenberg block - case study highlights

---
+++ web/wp-content/plugins/lf-cncf-blocks/src/case-study-highlights/block.js
@@ -0,0 +1,73 @@
+/**
+ * Register block JS
+ *
+ * @package WordPress
+ * @since 1.0.0
+ *
+ * @tags
+ * @phpcs:disable WordPress.WhiteSpace.OperatorSpacing.NoSpaceAfter
+ * @phpcs:disable WordPress.WhiteSpace.OperatorSpacing.NoSpaceBefore
+ * @phpcs:disable Generic.WhiteSpace.ScopeIndent.IncorrectExact
+ * @phpcs:disable Generic.WhiteSpace.ScopeIndent.Incorrect
+ * @phpcs:disable PEAR.Functions.FunctionCallSignature.Indent
+ */
+
+// Import CSS.
+import './editor.scss';
+import './style.scss';
+
+import Edit from './edit.js';
+
+const { __ } = wp.i18n;
+const { registerBlockType } = wp.blocks;
+
+/**
+ * Register: Gutenberg Block.
+ *
+ * Registers a new block provided a unique name and an object defining its
+ * behavior. Once registered, the block is made editor as an option to any
+ * editor interface where blocks are implemented.
+ *
+ * @link https://wordpress.org/gutenberg/handbook/block-api/
+ * @param  {string}   name     Block name.
+ * @param  {Object}   settings Block settings.
+ * @return {?WPBlock}          The block, if it has been successfully registered; otherwise `undefined`.
+ */
+registerBlockType(
+	'lf/case-study-highlights',
+	{
+		title: __( 'LF | Case Study Highlights' ),
+		description: __( 'A highlighted area in the page to showcase the results and changes in Case Studies' ),
+		icon: 'grid-view',
+		category: 'cncf',
+		keywords: [
+			__( 'case study' ),
+			__( 'case' ),
+			__( 'highlights' ),
+			__( 'cncf' ),
+		],
+		example: {
+			attributes: {
+				highlight01: 'DEPLOYMENT TIME Went from an hour to minutes',
+			} },
+		attributes: {
+			highlight01: {
+				type: 'string',
+				default: '',
+			},
+			highlight02: {
+				type: 'string',
+				default: '',
+			},
+			highlight03: {
+				type: 'string',
+				default: '',
+			},
+		},
+		html: false,
+		edit: Edit,
+		save() {
+			return null;
+		},
+	}
+);
---
+++ web/wp-content/plugins/lf-cncf-blocks/src/case-study-highlights/edit.js
@@ -0,0 +1,70 @@
+/**
+ * Edit screen code
+ *
+ * @package WordPress
+ * @since 1.0.0
+ *
+ * @tags
+ * @phpcs:disable WordPress.WhiteSpace.OperatorSpacing.NoSpaceAfter
+ * @phpcs:disable WordPress.WhiteSpace.OperatorSpacing.NoSpaceBefore
+ * @phpcs:disable Generic.WhiteSpace.ScopeIndent.IncorrectExact
+ * @phpcs:disable Generic.WhiteSpace.ScopeIndent.Incorrect
+ * @phpcs:disable PEAR.Functions.FunctionCallSignature.Indent
+ */
+
+const { __ } = wp.i18n;
+const { RichText } = wp.blockEditor || wp.editor;
+const { Component } = wp.element;
+
+class CaseStudyHighlight extends Component {
+	render() {
+		const { attributes, setAttributes } = this.props;
+		const { highlight01, highlight02, highlight03, className } = attributes;
+
+		return (
+			<div
+				className="case-study-highlights alignwide is-style-blue-pink-gradient ">
+				<div className="container case-study-highlights-wrapper">
+			<div><h3>CNCF Projects Used</h3>
+			<div>
+			<img src="https://via.placeholder.com/50x50/d9d9d9/000000" />&nbsp;
+			<img src="https://via.placeholder.com/50x50/d9d9d9/000000" />
+			</div>
+			</div>
+			<div>
+				<RichText
+					tagName="h3"
+					className={ className }
+					placeholder={ __( 'Example Text 1', 'cncf-blocks' ) }
+					keepPlaceholderOnFocus={ false }
+					value={ highlight01 }
+					onChange={ value => setAttributes( { highlight01: value } ) }
+				/>
+			</div>
+			<div>
+				<RichText
+					tagName="h3"
+					className={ className }
+					placeholder={ __( 'Example Text 2', 'cncf-blocks' ) }
+					keepPlaceholderOnFocus={ false }
+					value={ highlight02 }
+					onChange={ value => setAttributes( { highlight02: value } ) }
+				/>
+			</div>
+			<div>
+				<RichText
+					tagName="h3"
+					className={ className }
+					placeholder={ __( 'Example Text 3', 'cncf-blocks' ) }
+					keepPlaceholderOnFocus={ false }
+					value={ highlight03 }
+					onChange={ value => setAttributes( { highlight03: value } ) }
+				/>
+			</div>
+			</div>
+			</div>
+		);
+	}
+}
+
+export default CaseStudyHighlight;
