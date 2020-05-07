ce64e4a3066ca57cfc93ef6c5e3e3670f32cf568
James Hunt <jim@thetwopercent.co.uk>
2020-05-07 19:09:28 +0100

    Adding styles to Newsletter

--- web/wp-content/themes/cncf-theme/components/footer.php
+++ web/wp-content/themes/cncf-theme/components/footer.php
@@ -12,31 +12,21 @@
 $options = get_option( 'cncf-mu' );
 ?>
 
+<?php get_template_part( 'components/newsletter' ); ?>
+
 <footer class="footer">
-	<div class="container wrap">
-		<form id="sfmc-form" action="https://cloud.email.thelinuxfoundation.org/CNCF-Newsletter-Subscriber-Form">
-			<label for="FirstName" required="">
-				<input type="text" name="FirstName" placeholder="FIRST" required>
-			</label>
-			<label for="LastName" required="">
-				<input type="text" name="LastName" placeholder="LAST" required>
-			</label>
-			<label for="EmailAddress" required="">
-				<input type="email" name="EmailAddress" placeholder="EMAIL" required>
-			</label>
-			<button type="submit" id="submitbtn">SIGN UP</button>
-			<div data-callback="onSubmit" data-sitekey="6LdMldUUAAAAABG2vrZ1GT7Eo_TgI-UPlG14ocVH" class="g-recaptcha" data-size="invisible"></div>
-		</form>
-		<div id="message"></div>
-	</div>
-	<div class="container wrap">
-		<?php get_template_part( 'components/social-links' ); ?>
-		<div class="copyright">
-			<p>Copyright &copy; <?php echo esc_html( gmdate( 'Y' ) ); ?>
-				<?php echo wp_kses_data( $options['copyright_textarea'] ); ?>
-			</p>
-		</div>
+	<div class="container wrap copyright-social-wrapper">
+
+			<?php get_template_part( 'components/social-links' ); ?>
+
+			<div class="copyright-text">
+				<p>Copyright &copy; <?php echo esc_html( gmdate( 'Y' ) ); ?>
+					<?php echo wp_kses_data( $options['copyright_textarea'] ); ?>
+				</p>
+			</div>
+
 	</div>
 </footer>
 <?php get_template_part( 'components/back-to-top' ); ?>
+<?php get_template_part( 'components/cookie-banner' ); ?>
 <?php get_footer(); ?>
