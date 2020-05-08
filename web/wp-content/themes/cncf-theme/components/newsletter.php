53a93844fe111f7d6bba732497858ba02b6c650a
cjyabraham <cjyabraham@gmail.com>
2020-05-08 13:00:18 +0800

    changes to newsletter markup

--- web/wp-content/themes/cncf-theme/components/newsletter.php
+++ web/wp-content/themes/cncf-theme/components/newsletter.php
@@ -15,7 +15,7 @@
 		webinars, events & latest news from CNCF and The Linux Foundation. Free to join. Unsubscribe at any time.
 	</p>
 
-	<form id="sfmc-form" class="newsletter-form"
+	<form id="sfmc-form1" class="newsletter-form"
 		action="https://cloud.email.thelinuxfoundation.org/CNCF-Newsletter-Subscriber-Form">
 		<label for="FirstName" required>
 			<input type="text" name="FirstName" placeholder="Your First Name"
@@ -29,15 +29,13 @@
 			<input type="email" name="EmailAddress"
 				placeholder="Your Email Address" required>
 		</label>
-		<button type="submit" class="button stocky" id="submitbtn">Subscribe</button>
-		<div data-callback="onSubmit"
-			data-sitekey="6LdMldUUAAAAABG2vrZ1GT7Eo_TgI-UPlG14ocVH"
-			class="g-recaptcha" data-size="invisible"></div>
+		<button type="submit" class="button stocky" id="sfmc-submit1">Subscribe</button>
+		<div id="recaptcha-form1" style="display:none;"></div>
 	</form>
 	<p class="smaller-text">By submitting this form, you acknowledge that your
 		information is subject to The Linux Foundation’s <a
 			href="https://www.linuxfoundation.org/privacy/"
 			rel="norefferer noopener" target="_blank">Privacy Policy</a>.</p>
-	<div id="message"></div>
+	<div id="sfmc-message1"></div>
 	</div>
 </section>
