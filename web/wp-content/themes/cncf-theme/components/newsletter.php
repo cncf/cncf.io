bdc0af9c890dd92f94018e8ca934838847e27bcd
James Hunt <jim@thetwopercent.co.uk>
2020-05-20 19:54:17 +0100

    Adding screen reader text to newsletter form

--- web/wp-content/themes/cncf-theme/components/newsletter.php
+++ web/wp-content/themes/cncf-theme/components/newsletter.php
@@ -16,14 +16,17 @@
 	<form id="sfmc-form1" class="newsletter-form"
 		action="https://cloud.email.thelinuxfoundation.org/CNCF-Newsletter-Subscriber-Form">
 		<label for="FirstName" required>
+		<span class="screen-reader-text">First Name</span>
 			<input type="text" id="FirstName" name="FirstName" placeholder="Your First Name" autocomplete="given-name" spellcheck="false"
 				required>
 		</label>
 		<label for="LastName" required>
+		<span class="screen-reader-text">Last Name</span>
 			<input type="text" id="LastName" name="LastName" placeholder="Your Last Name" autocomplete="family-name" spellcheck="false"
 				required>
 		</label>
 		<label for="EmailAddress" required>
+		<span class="screen-reader-text">Email Address</span>
 			<input type="email" id="EmailAddress" name="EmailAddress"
 				placeholder="Your Email Address" autocomplete="email" spellcheck="false" required>
 		</label>
