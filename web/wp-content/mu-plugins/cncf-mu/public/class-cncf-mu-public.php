811a962f803411cbfe47f241e54b2dff2431eefd
cjyabraham <cjyabraham@gmail.com>
2020-05-21 09:41:11 +0800

    added GTM code instead of google analytics

--- web/wp-content/mu-plugins/cncf-mu/public/class-cncf-mu-public.php
+++ web/wp-content/mu-plugins/cncf-mu/public/class-cncf-mu-public.php
@@ -81,22 +81,38 @@ class Cncf_Mu_Public {
 	}
 
 	/**
-	 * Inserts Google Analytics code on live sites.
+	 * Inserts <head> Google Tag Manager code.
 	 */
-	public function insert_google_analytics() {
+	public function insert_gtm_head() {
 		$current_domain = parse_url( home_url(), PHP_URL_HOST );
 		$analytics_code = <<<EOD
-		<!-- Google Analytics -->
-		<script>
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-KNXFWV');</script>
+	<!-- End Google Tag Manager -->
 
-		ga('create', 'UA-831873-38', 'auto');
-		ga('send', 'pageview');
-		</script>
-		<!-- End Google Analytics -->
+	EOD;
+
+		if ( 'www.cncf.io' == $current_domain && ! is_user_logged_in() ) {
+			// this is a live site so output the analytics code.
+			echo $analytics_code; //phpcs:ignore
+		}
+	}
+
+	/**
+	 * Inserts the <body> Google Tag Manager code.
+	 */
+	public function insert_gtm_body() {
+		$current_domain = parse_url( home_url(), PHP_URL_HOST );
+		$analytics_code = <<<EOD
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KNXFWV"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+
 	EOD;
 
 		if ( 'www.cncf.io' == $current_domain && ! is_user_logged_in() ) {
@@ -117,7 +133,7 @@ class Cncf_Mu_Public {
 			// Used for analytics inserted by insert_google_analytics().
 			$hints[] = array(
 				'crossorigin' => '',
-				'href'        => '//www.google-analytics.com',
+				'href'        => '//www.googletagmanager.com',
 			);
 			// Used by ReCaptcha.
 			$hints[] = array(
