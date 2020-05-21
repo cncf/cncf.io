490fe81cd3776dd6a5991c97dc8c74d88793fde3
James Hunt <jim@thetwopercent.co.uk>
2020-05-21 21:39:32 +0100

    In The News styling, ref #83

--- web/wp-content/themes/cncf-theme/components/post-loop.php
+++ web/wp-content/themes/cncf-theme/components/post-loop.php
@@ -29,10 +29,15 @@ $image = new Image();
 
 				if ( $is_in_the_news_category ) :
 					$link_url = get_post_meta( get_the_ID(), 'cncf_post_external_url', true );
-					$target_attr = ' target="_blank"';
+
+					$target_attr = 'rel="noopener" target="_blank"';
+
+					$add_external_icon = ' external is-primary-color';
+
 					if ( ! $link_url ) {
-						$target_attr = '';
-						$link_url = get_the_permalink();
+						$target_attr       = '';
+						$link_url          = get_the_permalink();
+						$add_external_icon = '';
 					}
 					?>
 		<div class="archive-item in-news-item">
@@ -44,17 +49,15 @@ $image = new Image();
 							if ( has_post_thumbnail() ) {
 								echo wp_get_attachment_image( get_post_thumbnail_id(), 'newsroom-media-coverage', false, array( 'class' => 'newsroom-media-coverage' ) );
 
-							} elseif ( isset( $options['generic_thumb_id'] ) && $options['generic_thumb_id'] ) {
-								echo wp_get_attachment_image( $options['generic_thumb_id'], 'newsroom-image', false, array( 'class' => 'newsroom-image' ) );
 							} else {
 								echo '<img src="' . esc_url( get_stylesheet_directory_uri() )
-								. '/images/thumbnail-default.svg" alt="CNCF" class="newsroom-image"/>';
+								. '/images/thumbnail-default.svg" alt="CNCF Media Coverage" class="newsroom-media-coverage"/>';
 							}
 							?>
 				</a>
 			</div>
 			<div class="archive-text-wrapper">
-				<p class="archive-title"><a href="<?php echo esc_url( $link_url ); ?>"
+				<p class="archive-title"><a class="<?php echo esc_html( $add_external_icon ); ?>" href="<?php echo esc_url( $link_url ); ?>"
 					<?php echo esc_attr( $target_attr ); ?>
 					title="<?php the_title(); ?>">
 					<?php the_title(); ?>
