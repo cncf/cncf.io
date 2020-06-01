af5e1560eb8668cfa54383bc92ced7ae9637f570
James Hunt <jim@thetwopercent.co.uk>
2020-06-01 23:24:55 +0100

    Only show byline on Blog, closes #75

--- web/wp-content/themes/cncf-theme/components/post-single.php
+++ web/wp-content/themes/cncf-theme/components/post-single.php
@@ -9,87 +9,87 @@
 
 ?><section class="hero">
 	<div class="container wrap no-background">
-	<?php
-	// Category of the post.
-	$all_categories = get_the_category();
-	if ( $all_categories ) :
-		?>
+		<?php
+		// Category of the post.
+		$all_categories = get_the_category();
+		if ( $all_categories ) :
+			?>
 		<p class="hero-parent-link">
-		<?php
-		// Only get the first item in the array.
-		$category = array_shift( $all_categories );
-		// $category = $the_category->name;
-		echo '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" title="See Posts in ' . esc_html( $category->name ) . '">' . esc_html( $category->name ) . '</a>';
-		?>
+			<?php
+			// Only get the first item in the array.
+			$category = array_shift( $all_categories );
+			echo '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" title="See Posts in ' . esc_html( $category->name ) . '">' . esc_html( $category->name ) . '</a>';
+			?>
 		</p>
-		<?php
+			<?php
 	endif;
-	?>
-	<h1 class="hero-post-title" itemprop="headline">
-	<?php
-	the_title();
-	?>
-</h1>
+		?>
+		<h1 class="hero-post-title" itemprop="headline">
+			<?php
+			the_title();
+			?>
+		</h1>
 	</div>
 </section>
 <main class="newsroom-single">
 	<article class="container wrap">
-	<?php
-	while ( have_posts() ) :
-		the_post();
-		?>
-
 		<?php
-		// Get the Category Author terms associated if any.
-		$category_author_terms = get_the_terms( $post->ID, 'cncf-author-category' );
-
-		if ( $category_author_terms ) {
-			// Only get the first item in the array.
-			$category_author_term = array_shift( $category_author_terms );
-			$category_author      = $category_author_term->name;
-		}
-
-		if ( ! empty( $category_author ) ) :
-
+		while ( have_posts() ) :
+			the_post();
 			?>
-	<div
-			class="skew-box secondary centered">CNCF
+
 			<?php
-			echo esc_html( $category_author );
-			?>
+			// Get the Category Author.
+			$category_author = Cncf_Utils::get_term_names( get_the_ID(), 'cncf-author-category', true );
+
+			// Get the guest author meta.
+			$guest_author = get_post_meta( get_the_ID(), 'cncf_post_guest_author', true );
+
+			if ( $category_author ) :
+				?>
+		<div class="skew-box secondary centered">CNCF
+				<?php
+				echo esc_html( $category_author );
+				?>
 			Blog Post</div>
-			<?php
-endif;
-		?>
-<p class="newsroom-single-meta">Posted on
-		<?php
-		the_date();
-		?>
-</p>
-		<?php
-		if ( ! has_block( 'lf/guest-author' ) ) :
+				<?php
+	endif;
 			?>
-	<p
-			class="newsroom-single-meta">
+		<p class="newsroom-single-meta date-icon">Posted on
 			<?php
-			echo 'By ' . get_the_author();
+			the_date();
 			?>
-			</p>
+		</p>
+
 			<?php
-endif;
-		?>
-<div class="entry-content">
-		<?php
-		the_content();
-		get_template_part( 'components/social-share' );
-		?>
+			if ( in_category( 'blog' ) ) :
+
+				if ( $guest_author ) {
+					?>
+		<p class="newsroom-guest-author">
+					<?php echo esc_html( $guest_author ); ?></p>
+					<?php
+				} else {
+					?>
+		<p class="newsroom-single-meta">
+			By <?php echo get_the_author(); ?></p>
+					<?php
+				}
+			endif;
+			?>
+
+		<div class="entry-content">
+			<?php
+			the_content();
+			get_template_part( 'components/social-share' );
+			?>
 			<hr class="newsroom-single-hr">
 			<?php
 			get_template_part( 'components/post-pagination' );
 			?>
-			</div>
+		</div>
 			<?php
 endwhile;
-	?>
-									</article>
+		?>
+	</article>
 </main>
