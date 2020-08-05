2e59eb5ffdf526ae45845f2ad3cb5c49eb3bb371
cjyabraham <cjyabraham@gmail.com>
2020-08-05 18:45:27 +0800

    inserted git commit info into slack notification

--- .circleci/set-up-globals.sh
+++ .circleci/set-up-globals.sh
@@ -31,6 +31,12 @@ else
 	fi
 fi
 echo 'export TERMINUS_ENV=${TERMINUS_ENV:-$DEFAULT_ENV}' >> $BASH_ENV
+
+# Store github info
+echo 'export COMMIT_MESSAGE=$(git log --format="%s" -n 1 $CIRCLE_SHA1)' >> $BASH_ENV
+echo 'export COMMIT_AUTHOR=$(git log --format="%an" -n 1 $CIRCLE_SHA1)' >> $BASH_ENV
+echo 'export COMMIT_HASH=$(git log --format="%h" -n 1 $CIRCLE_SHA1)' >> $BASH_ENV
+
 source $BASH_ENV
 
 #===========================================
