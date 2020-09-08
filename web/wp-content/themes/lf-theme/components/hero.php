cc23263c4985ea86a02610e153b3a9800e3a29f7
cjyabraham <cjyabraham@gmail.com>
2020-09-08 10:46:11 +0800

    Show appropriate title in author category (#316)

--- web/wp-content/themes/lf-theme/components/hero.php
+++ web/wp-content/themes/lf-theme/components/hero.php
@@ -33,39 +33,43 @@ $options = get_option( 'lf-mu' );
 		<div>
 			<?php
 			if ( function_exists( 'is_tag' ) && is_tag() || is_category() || is_tax() ) :
-				?>
-			<h1 class="blog-title"><?php single_cat_title(); ?></h1>
-				<?php
+				if ( is_tax( 'lf-author-category' ) ) {
+					?>
+					<h1 class="blog-title">Author category: <?php single_cat_title(); ?></h1>
+					<?php					
+				} else {
+					?>
+					<h1 class="blog-title"><?php single_cat_title(); ?></h1>
+					<?php	
+				}
 				the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
-				<?php elseif ( is_author() ) : ?>
-			<h1 class="blog-title">All posts by <?php the_author(); ?></h1>
+			<?php elseif ( is_author() ) : ?>
+				<h1 class="blog-title">All posts by <?php the_author(); ?></h1>
 			<?php elseif ( is_archive() ) : ?>
-			<h1 class="blog-title"><a href="<?php the_permalink(); ?>"
-					rel="bookmark"
-					title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-			</h1>
+				<h1 class="blog-title"><a href="<?php the_permalink(); ?>"
+						rel="bookmark"
+						title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+				</h1>
 				<?php the_archive_description( '<div class="taxonomy-description">', '</div>' ); ?>
 			<?php elseif ( is_search() ) : ?>
-			<h2 class="page-title"><span>Search results for: </span>
+				<h2 class="page-title"><span>Search results for: </span>
 				<?php echo esc_attr( get_search_query() ); ?></h2>
 			<?php elseif ( ! ( is_404() ) && ( is_page() ) ) : ?>
-			<h1 class="page-title" itemprop="headline"><?php the_title(); ?>
+				<h1 class="page-title" itemprop="headline"><?php the_title(); ?>
 			</h1>
 			<?php elseif ( ! ( is_404() ) && ( is_single() ) ) : ?>
-			<h1 class="post-title" itemprop="headline"><?php the_title(); ?>
-			</h1>
+				<h1 class="post-title" itemprop="headline"><?php the_title(); ?>
+				</h1>
 			<?php elseif ( is_404() ) : ?>
 				<h2 class="post-title" itemprop="headline">Sorry that page wasn't
 				found</h2>
 			<?php elseif ( is_home() ) : ?>
-			<h2 class="blog-title"><?php single_post_title(); ?></h2>
+				<h2 class="blog-title"><?php single_post_title(); ?></h2>
 			<?php else : ?>
-			<h1 class="page-title" itemprop="headline"><?php the_title(); ?>
-			</h1>
-				<?php
-endif;
-			?>
+				<h1 class="page-title" itemprop="headline"><?php the_title(); ?>
+				</h1>
+			<?php endif; ?>
 		</div>
 	</div>
 </section>
