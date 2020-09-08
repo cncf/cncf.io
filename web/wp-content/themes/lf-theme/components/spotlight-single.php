9808c602fb80ba8444d05c14d37276562e991f34
cjyabraham <cjyabraham@gmail.com>
2020-09-08 13:15:11 +0800

    made the spotlight skew clickable

--- web/wp-content/themes/lf-theme/components/spotlight-single.php
+++ web/wp-content/themes/lf-theme/components/spotlight-single.php
@@ -7,8 +7,10 @@
  * @since 1.0.0
  */
 
- // get author category.
- $spotlight_type = Lf_Utils::get_term_names( get_the_ID(), 'lf-spotlight-type', true );
+// get author category.
+$spotlight_type = Lf_Utils::get_term_names( get_the_ID(), 'lf-spotlight-type', true );
+$spotlight_type_slug = Lf_Utils::get_term_slugs( get_the_ID(), 'lf-spotlight-type', true );
+
 ?>
 <section class="hero">
 	<div class="container wrap no-background">
@@ -28,9 +30,12 @@
 			the_post();
 			?>
 
-		<?php if ( $spotlight_type ) : ?>
-		<div class="skew-box secondary centered margin-bottom-large">CNCF
-				<?php echo esc_html( $spotlight_type ); ?> Spotlight</div>
+			<?php
+			if ( $spotlight_type ) :
+				$spotlight_type_link = '/spotlights/?_sft_lf-spotlight-type=' . $spotlight_type_slug;
+				?>
+		<a class="skew-box secondary centered margin-bottom-small" href="<?php echo esc_url( $spotlight_type_link ); ?>">CNCF
+				<?php echo esc_html( $spotlight_type ); ?> Spotlight</a>
 		<?php endif; ?>
 
 		<p><span class="posted-date date-icon">Posted on
