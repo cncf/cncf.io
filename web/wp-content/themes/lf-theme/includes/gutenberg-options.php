1c3c555095f1cb5640e79cb9bfb4066aef79aa15
James Hunt <jim@thetwopercent.co.uk>
2020-09-09 22:29:33 +0200

    Fix: Adding default typography size properly via fontSize, closes #315

--- web/wp-content/themes/lf-theme/includes/gutenberg-options.php
+++ web/wp-content/themes/lf-theme/includes/gutenberg-options.php
@@ -13,15 +13,15 @@
   * Block Template - Page
   */
 function lf_page_block_template() {
-
-	$page_type_object = get_post_type_object( 'page' );
-
+	$page_type_object           = get_post_type_object( 'page' );
 	$page_type_object->template = array(
 		array(
 			'core/paragraph',
 			array(
-				'placeholder'   => 'This first introductory paragraph of text should be H3 size lorem ipsum dolor sit amet consectetuer adipiscing elit aenean commodo',
-				'className' => 'has-header-3-font-size is-style-max-900',
+				'placeholder' => 'This first introductory paragraph of text should be H3 size lorem ipsum dolor sit amet consectetuer adipiscing elit aenean commodo',
+				'className'   => 'is-style-max-width-900',
+				'fontSize'   => 'header-3',
+
 			),
 		),
 		array(
@@ -30,8 +30,6 @@ function lf_page_block_template() {
 				'placeholder' => 'And then change to normal paragraph text lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula get dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate get, arcu. In enim justo, rhoncus ut imperdiet a.',
 			),
 		),
-
 	);
-
 }
 add_action( 'init', 'lf_page_block_template' );
