6c872d8b2b9e8b193da9e1e284ccc77e7f989404
James Hunt <jim@thetwopercent.co.uk>
2020-09-29 00:34:52 +0200

    Removing tags from posts

--- web/wp-content/themes/lf-theme/functions.php
+++ web/wp-content/themes/lf-theme/functions.php
@@ -225,3 +225,11 @@ foreach ( $regex_json_path_patterns as $regex_json_path_pattern ) {
 		break;
 	}
 }
+
+/**
+ * Remove tags support from posts
+ */
+function myprefix_unregister_tags() {
+	unregister_taxonomy_for_object_type( 'post_tag', 'post' );
+}
+add_action( 'init', 'myprefix_unregister_tags' );
