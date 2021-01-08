8832630b753b7da39e5dab4a9630cab6526087ef
James Hunt <jim@thetwopercent.co.uk>
2021-01-08 19:36:06 +0000

    Adding new template that removes hero (reports)

---
+++ web/wp-content/themes/lf-theme/templates/full-width-without-hero.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: Full-width page without hero
+ * Template Post Type: page
+ *
+ * @package WordPress
+ * @subpackage lf-theme
+ * @since 1.0.0
+ */
+
+get_template_part( 'components/header' );
+
+?>
+<main class="page-content">
+	<article class="container-full-width wrap entry-content">
+		<?php
+		if ( have_posts() ) :
+			while ( have_posts() ) :
+				the_post();
+				 the_content();
+			endwhile;
+		endif;
+		?>
+	</article>
+</main>
+
+<?php
+get_template_part( 'components/footer' );
