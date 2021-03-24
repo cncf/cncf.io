0cae04b5e8e0114a4a4262e8b70a236def938a38
cjyabraham <cjyabraham@gmail.com>
2021-03-24 15:10:07 +0800

    fixed up kubeweekly single format

--- web/wp-content/themes/lf-theme/components/kubeweekly-single.php
+++ web/wp-content/themes/lf-theme/components/kubeweekly-single.php
@@ -10,6 +10,9 @@
 ?>
 <section class="hero">
 	<div class="container wrap no-background">
+
+		<div class="wp-block-columns">
+		<div class="wp-block-column" style="flex-basis:60%">
 		<p class="hero-parent-link">
 			<a href="<?php echo esc_url( get_home_url() ); ?>/kubeweekly/" title="See all Kubeweeklys">Kubeweekly</a>
 		</p>
@@ -18,66 +21,41 @@
 			the_title();
 			?>
 		</h1>
+		<p class="date-author-row"><span
+			class="posted-date date-icon">Posted on
+			<?php
+			the_date();
+			?>
+		</span>
+		</p>
+		<div style="height:160px" aria-hidden="true" class="wp-block-spacer is-style-60-responsive"></div>
+		</div>
+
+		<div class="wp-block-column" style="flex-basis:40%">
+		<div class="wp-block-group has-white-color has-tertiary-400-background-color has-text-color has-background"><div class="wp-block-group__inner-container">
+
+		<h4>Join the KubeWeekly mailing list</h4>
+		<?php echo do_shortcode( '[hubspot type=form portal=8112310 id=cf924a1f-5b8b-40dc-9452-b207c494dae2]' ); ?>
+		<p class="has-small-font-size">By submitting this form, you acknowledge that your information is subject to The Linux Foundation’s <a href="https://www.linuxfoundation.org/privacy/" rel="noopener" class="external" target="_blank">Privacy Policy</a>.</p>
+
+		</div></div>
+		</div>
+		</div>
+
 	</div>
 </section>
 <main class="newsroom-single">
-	<section class="sidebar-newsletter">
-		<?php
-		get_template_part( 'components/kubeweekly-newsletter' );
-		?>
-	</section>
 	<article class="container wrap">
+	<div class="entry-content post-content">
 		<?php
-		while ( have_posts() ) :
-			the_post();
-			?>
-
-			<?php
-			// Get the Category Author.
-			$category_author = Lf_Utils::get_term_names( get_the_ID(), 'lf-author-category', true );
-			$category_author_slug = Lf_Utils::get_term_slugs( get_the_ID(), 'lf-author-category', true );
-
-			if ( $category_author ) :
-				$category_author_link = '/lf-author-category/' . $category_author_slug . '/';
-				?>
-				<a class="skew-box secondary centered margin-bottom-small" title="See more content from <?php echo esc_attr( $category_author ); ?>" href="<?php echo esc_url( $category_author_link ); ?>">CNCF
-				<?php
-				echo esc_html( $category_author );
-				?>
-				Blog Post</a>
-				<?php
-		endif;
-			?>
-			<p class="date-author-row"><span
-				class="posted-date date-icon">Posted on
-				<?php
-				the_date();
-				?>
-			</span>
-			<?php
-			// Post author.
-			if ( in_category( 'blog' ) ) :
-				echo wp_kses_post( Lf_Utils::display_author( get_the_ID(), true ) );
-				?>
-			</span>
-				<?php
-		endif;
-			?>
-		</p>
-
-		<div class="entry-content post-content">
-			<?php
-			the_content();
-			get_template_part( 'components/social-share' );
-			?>
-			<hr class="hr-light">
-			<?php
-			get_template_part( 'components/post-pagination' );
-			?>
-		</div>
-			<?php
-endwhile;
+		the_content();
+		get_template_part( 'components/social-share' );
 		?>
+		<hr class="hr-light">
+		<?php
+		get_template_part( 'components/post-pagination' );
+		?>
+	</div>
 	</article>
 </main>
 
