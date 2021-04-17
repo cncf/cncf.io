781d03d05ce5f2c778cdfb074e2e60eb27d1ed63
cjyabraham <cjyabraham@gmail.com>
2021-04-17 15:25:48 +0800

    fix whitespace

--- web/wp-content/themes/lf-theme/includes/shortcode-whoweare-metrics.php
+++ web/wp-content/themes/lf-theme/includes/shortcode-whoweare-metrics.php
@@ -19,53 +19,52 @@ function add_whoweare_metrics_shortcode( $atts ) {
 	?>
 <section data-element="count-up-block" class="count-up-block" style="--text-main-color: #FFFFFF;">
 	<div class="count-up-wrapper">
-				<div class="count-up-column">
-					<a class="no-decoration" target="_blank" rel="noopener" href="https://all.devstats.cncf.io/">
-				
-						<div class="icon-wrap">
-							<img src="https://www.cncf.io/wp-content/uploads/2020/05/icon-computer.svg" class="attachment-medium size-medium" alt="Computer" loading="lazy" height="74.18562" width="118.40117">
-						</div>
-						<div class="text-wrap" data-mh="facts-text-wrap">
-							<div class="number number-item h2"><?php echo esc_html( round( $metrics['contributors'] / 1000 ) ); ?>K+</div>
-							<span class="count-up-description"># of contributors to CNCF projects</span>
-						</div>
-					</a>
+		<div class="count-up-column">
+			<a class="no-decoration" target="_blank" rel="noopener" href="https://all.devstats.cncf.io/">		
+				<div class="icon-wrap">
+					<img src="https://www.cncf.io/wp-content/uploads/2020/05/icon-computer.svg" class="attachment-medium size-medium" alt="Computer" loading="lazy" height="74.18562" width="118.40117">
 				</div>
+				<div class="text-wrap" data-mh="facts-text-wrap">
+					<div class="number number-item h2"><?php echo esc_html( round( $metrics['contributors'] / 1000 ) ); ?>K+</div>
+					<span class="count-up-description"># of contributors to CNCF projects</span>
+				</div>
+			</a>
+		</div>
 
-				<div class="count-up-column">
-						<a class="no-decoration" href="https://www.cncf.io/certification/training/">
-				
-								<div class="icon-wrap">
-					<img src="https://www.cncf.io/wp-content/uploads/2020/05/icon-rocket.svg" class="attachment-medium size-medium" alt="Rocket" loading="lazy" height="92.41077" width="92.41107">				</div>
-								<div class="text-wrap" data-mh="facts-text-wrap">
-					<div class="number number-item h2">633</div>
-										<span class="count-up-description">CNCF Members</span>
-										</div>
-							</a>
-							</div>
-				<div class="count-up-column">
-						<a class="no-decoration" href="https://www.cncf.io/certification/software-conformance/">
-				
-								<div class="icon-wrap">
-					<img src="https://www.cncf.io/wp-content/uploads/2020/05/icon-settings.svg" class="attachment-medium size-medium" alt="Settings" loading="lazy" height="94.09873" width="110.60081">				</div>
-								<div class="text-wrap" data-mh="facts-text-wrap">
-					<div class="number number-item h2">104</div>
-										<span class="count-up-description">Certified Kubernetes Distributions &amp; Platforms</span>
-										</div>
-							</a>
-							</div>
-				<div class="count-up-column">
-						<a class="no-decoration" target="_blank" rel="noopener" href="https://www.meetup.com/pro/cncf/">
-				
-								<div class="icon-wrap">
-					<img src="https://www.cncf.io/wp-content/uploads/2020/05/icon-members.svg" class="attachment-medium size-medium" alt="Users" loading="lazy" height="106.10079" width="140.8399">				</div>
-								<div class="text-wrap" data-mh="facts-text-wrap">
+		<div class="count-up-column">
+			<a class="no-decoration" href="https://www.cncf.io/certification/training/">		
+				<div class="icon-wrap">
+					<img src="https://www.cncf.io/wp-content/uploads/2020/05/icon-rocket.svg" class="attachment-medium size-medium" alt="Rocket" loading="lazy" height="92.41077" width="92.41107">
+				</div>
+				<div class="text-wrap" data-mh="facts-text-wrap">
+					<div class="number number-item h2">633+</div>
+					<span class="count-up-description">CNCF Members</span>
+				</div>
+			</a>
+		</div>
+		<div class="count-up-column">
+			<a class="no-decoration" href="https://www.cncf.io/certification/software-conformance/">				
+				<div class="icon-wrap">
+					<img src="https://www.cncf.io/wp-content/uploads/2020/05/icon-settings.svg" class="attachment-medium size-medium" alt="Settings" loading="lazy" height="94.09873" width="110.60081">
+				</div>
+				<div class="text-wrap" data-mh="facts-text-wrap">
+					<div class="number number-item h2">104+</div>
+					<span class="count-up-description">Certified Kubernetes Distributions &amp; Platforms</span>
+				</div>
+			</a>
+		</div>
+		<div class="count-up-column">
+			<a class="no-decoration" target="_blank" rel="noopener" href="https://www.meetup.com/pro/cncf/">				
+				<div class="icon-wrap">
+					<img src="https://www.cncf.io/wp-content/uploads/2020/05/icon-members.svg" class="attachment-medium size-medium" alt="Users" loading="lazy" height="106.10079" width="140.8399">
+				</div>
+				<div class="text-wrap" data-mh="facts-text-wrap">
 					<div class="number number-item h2">163K+</div>
-										<span class="count-up-description">CNCF Meetup members</span>
-										</div>
-							</a>
-							</div>
-			</div>
+					<span class="count-up-description">CNCF Meetup members</span>
+				</div>
+			</a>
+		</div>
+	</div>
 </section>
 	<?php
 	$block_content = ob_get_clean();
