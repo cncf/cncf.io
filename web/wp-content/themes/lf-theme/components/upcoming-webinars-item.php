be1b518083a34b91e7e7eb7860d93f3cda82351d
cjyabraham <cjyabraham@gmail.com>
2021-04-28 15:03:11 +0800

    ensure we don't show a webinar on the homepage that has passed (#411); change to "online programs"

--- web/wp-content/themes/lf-theme/components/upcoming-webinars-item.php
+++ web/wp-content/themes/lf-theme/components/upcoming-webinars-item.php
@@ -40,7 +40,7 @@ if ( $show_images ) :
 			Lf_Utils::display_responsive_images( get_post_thumbnail_id(), 'newsroom-540', '540px', 'archive-image' );
 		} else {
 			echo '<img loading="lazy" src="' . esc_url( get_stylesheet_directory_uri() )
-			. '/images/webinar-default.jpg" alt="' . esc_attr( lf_blocks_get_site() ) . '" class="archive-image"/>';
+			. '/images/online-program-thumb.jpg" alt="' . esc_attr( lf_blocks_get_site() ) . '" class="archive-image"/>';
 		}
 		?>
 	</div>
