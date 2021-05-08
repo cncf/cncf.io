5a03585b5cd8680e95108879b6764c230a273930
cjyabraham <cjyabraham@gmail.com>
2021-05-08 12:07:56 +0800

    reset position of kubecon homepage promo and set kubecon na info

--- web/wp-content/themes/lf-theme/components/wip/home-event-highlight.php
+++ web/wp-content/themes/lf-theme/components/wip/home-event-highlight.php
@@ -14,17 +14,22 @@
 <div class="container wrap event-highlight-wrapper">
 
 <div>
-<h2>May 4-7, 2021</h2>
-<p class="h5">The CNCF’s flagship conference virtually gathers adopters and technologists from leading open source and cloud native communities for four days of education and advancement of cloud native computing. <strong>#KubeCon + #CloudNativeCon</strong></p>
+<h2>October 12-15, 2021</h2>
+<p class="h5">The CNCF’s flagship conference gathers adopters and technologists from leading open source and cloud native communities for four days of education and advancement of cloud native computing. <strong>#KubeCon + #CloudNativeCon</strong></p>
 
-<a href="https://events.linuxfoundation.org/kubecon-cloudnativecon-europe/" class="button external" rel="noopener">Register Now</a>
+<a href="https://events.linuxfoundation.org/kubecon-cloudnativecon-north-america/" class="button external" rel="noopener">Register Now</a>
 
 </div>
-<div><a href="https://events.linuxfoundation.org/kubecon-cloudnativecon-europe/" rel="noopener"><img loading="lazy" src="https://events.linuxfoundation.org/wp-content/uploads/2021/03/KubeCon_EU_2021_web_web-logo.svg" alt="Kubecon 2021"></a></div>
+<div><a href="https://events.linuxfoundation.org/kubecon-cloudnativecon-north-america/" rel="noopener"><img loading="lazy" src="https://events.linuxfoundation.org/wp-content/uploads/2020/11/KubeCon_NA_2021_web_web-logo-white-1.svg" alt="Kubecon 2021"></a></div>
 
 <div>
-<figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
-<div class="wp-block-lf-youtube-lite"><lite-youtube videoid="I_rbIsM-otA"></lite-youtube></div>
+Highlights from
+KubeCon + CloudNativeCon<br>
+North America 2019
+<figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio">
+<div class="wp-block-embed__wrapper">
+<iframe loading="lazy" title="KubeCon + CloudNativeCon 2019 - San Diego Highlights" width="500" height="281" src="https://www.youtube.com/embed/56ftznhkdh0?feature=oembed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
 </div></figure>
 </div>
 
