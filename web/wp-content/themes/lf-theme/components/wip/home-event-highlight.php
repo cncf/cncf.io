9d62174ba3b9f6b3c180c93e79baf1ed74951d39
cjyabraham <cjyabraham@gmail.com>
2021-05-08 12:31:16 +0800

    tweaked the kubecon CTA

--- web/wp-content/themes/lf-theme/components/wip/home-event-highlight.php
+++ web/wp-content/themes/lf-theme/components/wip/home-event-highlight.php
@@ -17,10 +17,10 @@
 <h2>October 12-15, 2021</h2>
 <p class="h5">The CNCF’s flagship conference gathers adopters and technologists from leading open source and cloud native communities for four days of education and advancement of cloud native computing. <strong>#KubeCon + #CloudNativeCon</strong></p>
 
-<a href="https://events.linuxfoundation.org/kubecon-cloudnativecon-north-america/" class="button external" rel="noopener">Register Now</a>
+<a href="https://events.linuxfoundation.org/kubecon-cloudnativecon-north-america/" class="button external" target="_blank" rel="noopener">Learn more</a>
 
 </div>
-<div><a href="https://events.linuxfoundation.org/kubecon-cloudnativecon-north-america/" rel="noopener"><img loading="lazy" src="https://events.linuxfoundation.org/wp-content/uploads/2020/11/KubeCon_NA_2021_web_web-logo-white-1.svg" alt="Kubecon 2021"></a></div>
+<div><a href="https://events.linuxfoundation.org/kubecon-cloudnativecon-north-america/" target="_blank" rel="noopener"><img loading="lazy" src="https://events.linuxfoundation.org/wp-content/uploads/2020/11/KubeCon_NA_2021_web_web-logo-white-1.svg" alt="Kubecon 2021"></a></div>
 
 <div>
 Highlights from
