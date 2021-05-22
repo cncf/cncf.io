2731258db60d5d16723132d33f10e552e42fc3cf
cjyabraham <cjyabraham@gmail.com>
2021-05-22 12:35:19 +0800

    got the url pointing correctly to case study

--- web/wp-content/themes/lf-theme/includes/shortcode-homepage-casestudies.php
+++ web/wp-content/themes/lf-theme/includes/shortcode-homepage-casestudies.php
@@ -27,10 +27,12 @@ function homepage_casestudies_shortcode( $atts ) {
 	ob_start();
 	$ids = explode( ',', $atts['ids'] );
 	shuffle( $ids );
-	$title = get_post_meta( $ids[0], 'lf_case_study_long_title', true );
-	$logo = get_post_meta( $ids[0], 'lf_case_study_company_logo', true );
+
+	$title    = get_post_meta( $ids[0], 'lf_case_study_long_title', true );
+	$logo     = get_post_meta( $ids[0], 'lf_case_study_company_logo', true );
 	$logo_url = wp_get_attachment_image_src( $logo );
-	$image = get_post_meta( $ids[0], 'lf_case_study_homepage_image', true );
+	$image    = get_post_meta( $ids[0], 'lf_case_study_homepage_image', true );
+	$url      = get_permalink( $ids[0] );
 	if ( ! $image ) {
 		// use the regular listing background image if no homepage image exists.
 		$image = get_post_thumbnail_id( $ids[0] );
@@ -50,16 +52,16 @@ function homepage_casestudies_shortcode( $atts ) {
 	<div style="height:60px" aria-hidden="true" class="wp-block-spacer is-style-60-responsive"></div>
 		<p class="h5 fw-400">CNCF projects are trusted by organizations around the world</p>
 			<div style="height:20px" aria-hidden="true" class="wp-block-spacer is-style-20-responsive"></div>
-		<a href="/case-studies/ericsson/">
+		<a href="<?php echo esc_url( $url ); ?>">
 		<?php
 		$image = new Image();
 		?>
 		<img loading="eager" src="<?php echo esc_url( $logo_url[0] ); ?>" alt="<?php echo esc_attr( $company ); ?>" width="300" height="64"></a>
 		<div style="height:20px" aria-hidden="true" class="wp-block-spacer is-style-20-responsive"></div>
-		<h2><a  class="has-white-color has-text-color" href="/case-studies/ericsson/">
+		<h2><a  class="has-white-color has-text-color" href="<?php echo esc_url( $url ); ?>">
 		<?php echo esc_html( $title ); ?>
 		</a></h2>
-		<a href="/case-studies/ericsson/" class="button">Read <?php echo esc_html( $company ); ?> Case Study</a>
+		<a href="<?php echo esc_url( $url ); ?>" class="button">Read <?php echo esc_html( $company ); ?> Case Study</a>
 		<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
 	</div>
 
