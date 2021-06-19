52c5c198461df2804d3d14419a650b53ecb15e21
cjyabraham <cjyabraham@gmail.com>
2021-06-19 16:18:25 +0800

    update Kubecon dates

--- web/wp-content/themes/lf-theme/includes/shortcode-home.php
+++ web/wp-content/themes/lf-theme/includes/shortcode-home.php
@@ -559,7 +559,7 @@ function homepage_event_highlight_shortcode() {
 <div class="container wrap event-highlight-wrapper">
 
 <div>
-<h2>October 12-15, 2021</h2>
+<h2>October 11-15, 2021</h2>
 <p class="h5">The CNCF’s flagship conference gathers adopters and technologists from leading open source and cloud native communities for four days of education and advancement of cloud native computing. <strong>#KubeCon + #CloudNativeCon</strong></p>
 
 <a href="https://events.linuxfoundation.org/kubecon-cloudnativecon-north-america/" class="button external" target="_blank" rel="noopener">Learn more</a>
