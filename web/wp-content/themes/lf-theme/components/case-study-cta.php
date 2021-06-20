ad5fa7ba29ac97d73f2e44b8400148cb68ee1627
James Hunt <jim@thetwopercent.co.uk>
2021-06-20 16:15:56 +0700

    Adding case study CTA, ref #433

---
+++ web/wp-content/themes/lf-theme/components/case-study-cta.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Case Study CTA
+ *
+ * @package WordPress
+ * @subpackage lf-theme
+ * @since 1.0.0
+ */
+
+?>
+
+<section class="case-study-cta">
+<div class="container wrap">
+<!-- wp:spacer {"height":80,"className":"is-style-80-responsive"} -->
+<div style="height:80px" aria-hidden="true" class="wp-block-spacer is-style-80-responsive"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:media-text {"mediaPosition":"right","mediaId":62498,"mediaLink":"/end-users/","linkDestination":"custom","mediaType":"image","imageFill":true,"focalPoint":{"x":"0.04","y":"1.00"},"backgroundColor":"tertiary-400","className":"is-style-default"} -->
+<div class="wp-block-media-text has-media-on-the-right is-stacked-on-mobile is-image-fill is-style-default has-tertiary-400-background-color has-background"><figure class="wp-block-media-text__media" style="background-image:url(/wp-content/uploads/2021/05/end-users.jpg);background-position:4% 100%"><a href="/enduser/"><img src="/wp-content/uploads/2021/05/end-users.jpg" alt="" class="wp-image-62498 size-full"/></a></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…","textColor":"white","fontSize":"header-4"} -->
+<p class="has-white-color has-text-color has-header-4-font-size"><strong>Join the CNCF end user community</strong> and accelerate your cloud native technology adoption in close collaboration with peers, project maintainers and CNCF.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link" href="/enduser/">Explore the benefits of the end user community</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:media-text -->
+</div>
+</section>
