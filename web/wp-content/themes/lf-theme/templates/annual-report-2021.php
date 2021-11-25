8bb882b463230ec6b358e111b85010d059f0bd2a
James Hunt <10615884+thetwopct@users.noreply.github.com>
2021-11-25 12:28:41 +0700

    Prepare annual report template and css page

---
+++ web/wp-content/themes/lf-theme/templates/annual-report-2021.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template Name: Annual Report 2021
+ * Template Post Type: page
+ *
+ * @package WordPress
+ * @subpackage lf-theme
+ * @since 1.0.0
+ */
+
+get_template_part( 'components/header' );
+
+wp_enqueue_style( '2021', get_template_directory_uri() . '/build/annual-report-2021.min.css', array(), filemtime( get_template_directory() . '/build/annual-report-2021.min.css' ), 'all' );
+
+if ( have_posts() ) :
+	while ( have_posts() ) :
+		the_post();
+		the_content();
+endwhile;
+endif;
+
+// wp_enqueue_script(
+// 	'annual-report-js',
+// 	get_stylesheet_directory_uri() . '/source/js/third-party/annual-report.js',
+// 	array( 'jquery' ),
+// 	filemtime( get_template_directory() . '/source/js/third-party/annual-report.js' ),
+// 	true
+// );
+
+get_template_part( 'components/footer' );
