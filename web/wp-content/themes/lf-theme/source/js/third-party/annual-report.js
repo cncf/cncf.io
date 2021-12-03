8de7a048670d6ffc3c6485e459689a096fe5dd55
James Hunt <10615884+thetwopct@users.noreply.github.com>
2021-12-03 08:57:57 +0700

    Implement new font, test of sticky menu idea

--- web/wp-content/themes/lf-theme/source/js/third-party/annual-report.js
+++ web/wp-content/themes/lf-theme/source/js/third-party/annual-report.js
@@ -1,4 +1,4 @@
-/**
+/** // phpcs:ignoreFile
  * Annual Report JS
  *
  * @package WordPress
@@ -11,149 +11,192 @@
 /* eslint-disable no-var */
 
 jQuery( document ).ready(
-	function( $ ) {
-		// If page loads with hash, go to it nicely after 1s.
-		if ( window.location.hash ) {
-			// smooth scroll to the anchor id if exists after 1s.
-			if ( $( window.location.hash ).length ) {
-				let theHash = $( window.location.hash );
-				let offsetNew = theHash === '#' ? 0 : theHash.offset().top - getSpacing();
-				setTimeout(
-					function() {
-						$( 'html, body' )
-							.animate(
-								{
-									scrollTop: offsetNew,
-								},
-								500
-							);
-					},
-					1000
-				);
-			}
-		}
+  function ( $ ) {
+    // If page loads with hash, go to it nicely after 1s.
+    if ( window.location.hash ) {
+      // smooth scroll to the anchor id if exists after 1s.
+      if ( $( window.location.hash ).length ) {
+        let theHash = $( window.location.hash );
+        let offsetNew = theHash === '#' ? 0 : theHash.offset().top - getSpacing();
+        setTimeout(
+          function () {
+            $( 'html, body' )
+              .animate(
+                {
+                  scrollTop: offsetNew,
+                },
+                500
+              );
+          },
+          1000
+        );
+      }
+    }
 
-		// create array of elements from nav links.
-		let topMenu = $( '.annual-report-sidebar .nav-items' );
-		if ( topMenu.length > 0 ) {
-			let lastId;
-			let menuItems = topMenu.find( 'a' );
-			let scrollItems = menuItems.map(
-				function() {
-					let item = $( $( this ).attr( 'href' ) );
-					if ( item.length ) {
-						return item;
-					}
-				}
-			);
+    // create array of elements from nav links.
+    let topMenu = $( '.annual-report-sidebar .nav-items' );
+    if ( topMenu.length > 0 ) {
+      let lastId;
+      let menuItems = topMenu.find( 'a' );
+      let scrollItems = menuItems.map(
+        function () {
+          let item = $( $( this ).attr( 'href' ) );
+          if ( item.length ) {
+            return item;
+          }
+        }
+      );
 
-			// Update nav and hash as user scrolls.
-			$( window ).on( 'scroll', window.utils.isThrottled( navUpdate, 200, true ) );
+      // Update nav and hash as user scrolls.
+      $( window ).on( 'scroll',window.utils.isThrottled( navUpdate,200,true ) );
 
-			// Click handler for menu items so we can get a fancy scroll animation.
-			menuItems.click(
-				function( e ) {
-					let href = $( this ).attr( 'href' );
-					let offsetTop = href === '#' ? 0 : $( href ).offset()
-						.top - getSpacing();
-					$( 'html, body' )
-						.stop()
-						.animate(
-							{
-								scrollTop: offsetTop,
-							},
-							500
-						);
-					e.preventDefault();
-				}
-			);
+      // Click handler for menu items so we can get a fancy scroll animation.
+      menuItems.click(
+        function ( e ) {
+          let href = $( this ).attr( 'href' );
+          let offsetTop = href === '#' ? 0 : $( href ).offset()
+            .top - getSpacing();
+          $( 'html, body' )
+            .stop()
+            .animate(
+              {
+                scrollTop: offsetTop,
+              },
+              500
+            );
+          e.preventDefault();
+        }
+      );
 
-			// Function to update nav and hash as user scrolls.
-			function navUpdate() {
-				let fromTop = $( this ).scrollTop();
+      // Function to update nav and hash as user scrolls.
+      function navUpdate() {
+        let fromTop = $( this ).scrollTop();
 
-				// Get id of current scroll item, add 20 for padding from header.
-				let cur = scrollItems.map(
-					function() {
-						if ( $( this ).offset().top < fromTop + getSpacing() + 20 ) {
-							return this;
-						}
-					}
-				);
+        // Get id of current scroll item, add 20 for padding from header.
+        let cur = scrollItems.map(
+          function () {
+            if ( $( this ).offset().top < fromTop + getSpacing() + 20 ) {
+              return this;
+            }
+          }
+        );
 
-				// Get the id of the current element.
-				cur = cur[ cur.length - 1 ];
-				let id = cur && cur.length ? cur[ 0 ].id : '';
+        // Get the id of the current element.
+        cur = cur[cur.length - 1];
+        let id = cur && cur.length ? cur[0].id : '';
 
-				if ( lastId !== id ) {
-					lastId = id;
-					// Set/remove active class.
-					menuItems
-						.parent()
-						.removeClass( 'is-active' )
-						.end()
-						.filter( "[href='#" + id + "']" )
-						.parent()
-						.addClass( 'is-active' );
-					if ( id ) {
-						if ( history.pushState ) {
-							window.history.replaceState(
-								null,
-								null,
-								'#' + id
-							);
-						} else {
-							// IE9, IE8, etc.
-							window.location.hash = '#!' + id;
-						}
-					} else {
-						removeHash();
-					}
-				}
-			}
-		}
+        if ( lastId !== id ) {
+          lastId = id;
+          // Set/remove active class.
+          menuItems
+            .parent()
+            .removeClass( 'is-active' )
+            .end()
+            .filter( "[href='#" + id + "']" )
+            .parent()
+            .addClass( 'is-active' );
+          if ( id ) {
+            if ( history.pushState ) {
+              window.history.replaceState(
+                null,
+                null,
+                '#' + id
+              );
+            } else {
+              // IE9, IE8, etc.
+              window.location.hash = '#!' + id;
+            }
+          } else {
+            removeHash();
+          }
+        }
+      }
+    }
 
-		// Get spacing required from top of window for content.
-		function getSpacing() {
-			let spacingTotal = 0;
-			let winH = $( window ).height();
-			let winW = $( window ).width();
-			const adminBar = $( '#wpadminbar' );
+    // Get spacing required from top of window for content.
+    function getSpacing() {
+      let spacingTotal = 0;
+      let winH = $( window ).height();
+      let winW = $( window ).width();
+      const adminBar = $( '#wpadminbar' );
 
-			if ( winH < 616 && winW > 514 ) {
-				spacingTotal += 40;
-			} else if ( winW < 800 ) {
-				spacingTotal += 80;
-			} else {
-				spacingTotal += 125;
-			}
-			if ( adminBar.length > 0 ) {
-				spacingTotal += 32;
-			}
-			return spacingTotal;
-		}
+      if ( winH < 616 && winW > 514 ) {
+        spacingTotal += 40;
+      } else if ( winW < 800 ) {
+        spacingTotal += 80;
+      } else {
+        spacingTotal += 125;
+      }
+      if ( adminBar.length > 0 ) {
+        spacingTotal += 32;
+      }
+      return spacingTotal;
+    }
 
-		// Remove hash from URL.
-		function removeHash() {
-			let scrollV;
-			let scrollH;
-			let loc = window.location;
+    // Remove hash from URL.
+    function removeHash() {
+      let scrollV;
+      let scrollH;
+      let loc = window.location;
 
-			if ( 'pushState' in history ) {
-				history.pushState( '', document.title, loc.pathname + loc.search );
-			} else {
-				// Prevent scrolling by storing the page's current scroll offset.
-				scrollV = document.body.scrollTop;
-				scrollH = document.body.scrollLeft;
+      if ( 'pushState' in history ) {
+        history.pushState( '',document.title,loc.pathname + loc.search );
+      } else {
+        // Prevent scrolling by storing the page's current scroll offset.
+        scrollV = document.body.scrollTop;
+        scrollH = document.body.scrollLeft;
 
-				loc.hash = '';
+        loc.hash = '';
 
-				// Restore the scroll offset, should be flicker free.
-				document.body.scrollTop = scrollV;
-				document.body.scrollLeft = scrollH;
-			}
-		}
+        // Restore the scroll offset, should be flicker free.
+        document.body.scrollTop = scrollV;
+        document.body.scrollLeft = scrollH;
+      }
+    }
 
-		// END.
-	}
+    // END.
+  }
 );
+
+function ready( fn ) {
+  if ( document.readyState !== "loading" ) {
+    fn();
+  } else {
+    document.addEventListener( "DOMContentLoaded",fn );
+  }
+}
+
+ready( function () {
+  fnAR2021Menu();
+} );
+
+
+
+function fnAR2021Menu() {
+  const trigger = document.querySelector( '.menu-trigger' )
+
+  const nav = document.querySelector( '.fake-nav' );
+
+  let scrollY = window.scrollY;
+
+  const observer = new IntersectionObserver( ( [entry] ) => {
+
+    if ( !entry.isIntersecting ) {
+      if ( window.scrollY > scrollY ) {
+        nav.classList.add( 'is-sticky' );
+      } else {
+        nav.classList.remove( 'is-sticky' );
+      }
+    } else {
+      // in view.
+      nav.classList.remove( 'is-sticky' );
+    }
+    scrollY = window.scrollY;
+  },{
+    root: null,
+    threshold: 0,
+  } )
+
+  observer.observe( trigger )
+
+}
