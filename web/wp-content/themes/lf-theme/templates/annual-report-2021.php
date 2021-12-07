d2f38df35188b8c3809092b337206b8499fcd35a
James Hunt <10615884+thetwopct@users.noreply.github.com>
2021-12-07 11:56:40 +0700

    Reset Annual Report page ready to code

--- web/wp-content/themes/lf-theme/templates/annual-report-2021.php
+++ web/wp-content/themes/lf-theme/templates/annual-report-2021.php
@@ -12,40 +12,17 @@ get_template_part( 'components/header' );
 
 wp_enqueue_style( '2021', get_template_directory_uri() . '/build/annual-report-2021.min.css', array(), filemtime( get_template_directory() . '/build/annual-report-2021.min.css' ), 'all' );
 ?>
-<main class="ar-2021 top">
-
-
-
+<main class="ar-2021">
 	<article class="container wrap">
-
-<img src="http://www.fillmurray.com/1200/500" alt="" height="500" width="100%">
-
-
-
 		<h3>WELCOME,<br>IT’S BEEN A YEAR<br>TO REMEMBER</h3>
-
-
-
 		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula get dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate get, arcu. In enim justo, rhoncus ut imperdiet a.</p>
-
-
-
 		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula get dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate get, arcu. In enim justo, rhoncus ut imperdiet a.</p>
-
-
-
 		<blockquote class="wp-block-quote is-style-max-width-800">
 			<p>Inspirational quote Lorem ipsum dolor sit amet consectetuer adipiscing elit aenean commodo</p>
 			<cite>John greenapples</cite>
 		</blockquote>
-
-
-
 		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula get dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate get, arcu. In enim justo, rhoncus ut imperdiet a.</p>
-
-
-
-		<div class="nav-el menu-trigger">
+		<div class="nav-el">
 			<div class="nav-box">
 				<a href="#who-we-are" class="box-link"></a>
 				<img src="https://via.placeholder.com/44x44/d9d9d9/000000"
@@ -86,100 +63,38 @@ wp_enqueue_style( '2021', get_template_directory_uri() . '/build/annual-report-2
 			</div>
 		</div>
 
-		<!-- fake box for menu  -->
-		<div class="nav-el fake-nav">
-			<div></div>
-			<div></div>
-		<div class="nav-box">
-				<img src="https://via.placeholder.com/44x44/d9d9d9/000000"
-					alt="">
-				<span>PAGE<BR>
-			NAVIGATION</span>
-				</div>
-			</div>
-
 		<h2 class="is-style-divider-line" id="who-we-are">Who We Are</h2>
-
-
 		<p
 			class="is-style-max-width-900 has-header-3-font-size">The Cloud Native Computing Foundation (CNCF) is an open source software foundation dedicated to making cloud native computing universal and sustainable. </p>
-
-
-
 		<p class="is-style-default"
 		>Cloud native technologies empower organizations to build and run scalable applications in modern, dynamic environments across public, private, and hybrid clouds. Containers, service meshes, microservices, immutable infrastructure, and declarative APIs exemplify the cloud native approach.</p>
-
-
-
 		<p
 			class="is-style-default">We are a community of open source projects, including Kubernetes, Prometheus, Envoy, and <a href="https://www.cncf.io/projects/">many others</a>. Kubernetes and other CNCF projects have quickly gained adoption and won community support, becoming some of the <a href="https://docs.google.com/presentation/d/1BoxFeENJcINgHbKfygXpXROchiRO2LBT-pzdaOFr4Zg/edit#slide=id.g39c264972c_182_493">highest velocity projects</a> in the history of open source.&nbsp;</p>
-
-
-
 		<p
 			class="is-style-default">CNCF employs <a href="https://www.cncf.io/people/staff/">34</a> people from various backgrounds and locations; 71% are women, 29% are men. CNCF’s Governance Leadership, comprising the Governing Board and Technical Oversight Committee, is 14% women and 86% men.</p>
-
-
-
 		<div style="height:100px" aria-hidden="true" class="wp-block-spacer">
 		</div>
-
-
-
 		<h2 class="is-style-divider-line" id="end-user-community">End User
 			Community</h2>
-
-
-
 		<p
 			class="is-style-max-width-900 has-header-3-font-size">CNCF prides itself on being an open source community driven by End Users. </p>
-
-
-
 		<p
 			class="is-style-default">End users are defined as companies that use cloud native technologies internally but do not sell any cloud native services externally. Companies that meet this definition are eligible to join the <a href="https://www.cncf.io/people/end-user-community/">End User Community</a>.&nbsp;</p>
-
-
-
 		<p
 			class="is-style-default">Our End User Community grew to over 145 members in 2020, indicating strong, continued interest in cloud native technologies. At present, the CNCF End User Community is the largest of any open source foundation.</p>
-
-
-
 		<p
 			class="is-style-default">The End User Community meets regularly and advises the CNCF Governing Board and Technical Oversight Committee (TOC) members on key challenges, emerging use cases, and areas of opportunity and new growth for cloud native technologies. End Users are also an important source for CNCF projects including Prometheus, Envoy, Argo, and Backstage.</p>
-
-
-
 		<p
 			class="is-style-default">A respondent of the <a href="https://github.com/cncf/surveys/tree/master/enduser/2020">2020 survey</a> commented that they “enjoy hearing other organizations discuss their experience implementing different flavors of the open source projects/software products. As we begin to mature our K8s offering, those conversations are very beneficial. Continue connecting end users and advocating for knowledge sharing and collaboration.” Among respondents, 100% would recommend CNCF to other companies, and <strong>the average satisfaction rating was 4.47 out of 5 (89%), up from 4.16 out of 5 in 2019 (83%)</strong>.&nbsp;</p>
-
-
-
 		<p
 			class="is-style-default">If you are using CNCF projects and meet the definition of an End User, we urge you to join our <a href="https://www.cncf.io/people/end-user-community/">End User Community</a> so you can participate in this influential group and provide your insights both to fellow end users and to the CNCF community as a whole. If you join, we are confident you will also learn from other end users who are deploying CNCF projects, praised as “a great way to share information between companies.”&nbsp;</p>
-
-
-
 		<div style="height:100px" aria-hidden="true"
 			class="wp-block-spacer is-style-100-responsive"></div>
-
-
-
 		<h2 class="is-style-divider-line" id="training">Training and
 			Certification</h2>
-
-
-
 		<p
 			class="is-style-max-width-100 has-header-3-font-size">CNCF’s training and certification program continued to grow this year. </p>
-
-
-
 		<p>In 2020, these training courses and exams received considerable interest:</p>
-
-
-
 		<ul>
 			<li><a href="https://www.cncf.io/certification/training/">Kubernetes
 					Massively Open Online Course (MOOC)</a> hit 165,000
@@ -197,18 +112,9 @@ wp_enqueue_style( '2021', get_template_directory_uri() . '/build/annual-report-2
 					Training Partner (KTP)</a> program grew to 50 certified
 				companies (40% increase from 2019).</li>
 		</ul>
-
-
-
 		<p
 			class="is-style-default">In November 2020, CNCF launched the <a href="https://www.cncf.io/certification/cks/">Certified Kubernetes Security Specialist (CKS)</a> exam. This exam will provide assurance that a certificant has the skills, knowledge, and competence on a broad range of best practices for securing container-based applications and Kubernetes platforms during build, deployment, and runtime.</p>
-
-
-
 		<p>Other courses that CNCF funded in 2020 include:</p>
-
-
-
 		<ul>
 			<li><a
 					href="https://training.linuxfoundation.org/announcements/service-mesh-fundamentals-training-course-now-available/">Service
@@ -226,139 +132,61 @@ wp_enqueue_style( '2021', get_template_directory_uri() . '/build/annual-report-2
 					href="https://www.edx.org/course/introduction-to-serverless-on-kubernetes">Intro
 					to Serverless on Kubernetes</a></li>
 		</ul>
-
-
-
 		<p></p>
-
-
-
 		<div style="height:100px" aria-hidden="true" class="wp-block-spacer">
 		</div>
-
-
-
 		<h2 class="is-style-divider-line" id="project-updates">Project Updates
 			and Satisfaction</h2>
-
-
-
 		<p
 			class="is-style-max-width-800 has-header-3-font-size">In 2020, CNCF-hosted projects Helm, Harbor, TikV, Rook, and etcd advanced to “graduated” status, for a total of 14 projects. </p>
-
-
-
 		<p
 			class="is-style-default">During 2020, Argo, Contour, and Operator Framework joined at the incubating level. Buildpacks, Cortex, Dragonfly, Falco, KubeEdge, SPIFFE/SPIRE, and Thanos joined our 20 incubating projects from the Sandbox level.&nbsp;</p>
-
-
-
 		<h3 id="project-maturity-levels">Project Maturity Levels</h3>
-
-
-
 		<p
 			class="is-style-default">CNCF projects are classified by maturity level, ranging from sandbox to incubating to graduated. CNCF uses these maturity levels to indicate to enterprises the degree of project readiness for enterprise adoption. </p>
-
-
-
 		<p>Graduated projects are suitable for the vast majority of enterprises. Incubating projects are suitable for early adopters, and sandbox projects are suitable for innovators. Projects increase their maturity level by demonstrating to the TOC that they have attained end user and vendor adoption, established a healthy rate of code commits and codebase changes, and attracted committers from multiple organizations. </p>
-
-
-
 		<div style="height:100px" aria-hidden="true" class="wp-block-spacer">
 		</div>
-
-
-
 		<h2 class="is-style-divider-line" id="community-engagement">Community
 			Engagement</h2>
-
-
-
 		<p
 			class="is-style-max-width-800 has-header-3-font-size">The CNCF community spans the world across our contributors, members, meetups, and ambassadors.</p>
-
-
-
 		<p
 			class="is-style-default">CNCF continues to support the development of this incredible cloud native community while also striving to ensure that everyone who participates feels welcome regardless of gender, gender identity, sexual orientation, disability, race, ethnicity, age, religion, or economic status. </p>
-
-
-
 		<p
 			class="is-style-default">In 2020, women and gender non-conforming speakers made up 74% of the keynotes at <a href="https://events.linuxfoundation.org/kubecon-cloudnativecon-europe/">KubeCon + CloudNativeCon EU Virtual</a> and 52% at <a href="https://events.linuxfoundation.org/kubecon-cloudnativecon-north-america/">KubeCon + CloudNativeCon NA Virtual</a>. At our KubeCon + CloudNativeCon events, there were a number of activities designed to foster the diversity of the cloud native community, including: peer group mentoring and career networking, the EmpowerUs event, a Diversity &amp; Inclusion workshop, KubeCon + CloudNativeCon diversity and need-based scholarships and complimentary registration to CNCF hosted co-located events.&nbsp;</p>
-
-
-
 		<p
 			class="is-style-default">CNCF offered scholarships to 717 diversity applicants from traditionally underrepresented and/or marginalized groups and 185 need-based applicants in 2020.&nbsp; Scholarships and diversity programs were funded by sponsorships from Amazon Web Services, CarGurus, Cloud Native Computing Foundation, ITRenew, Legacy II Cloud, Palo Alto Networks, Two Sigma, and VMware.&nbsp;</p>
-
-
-
 		<p
 			class="is-style-default">Including the 2020 virtual events, CNCF has offered more than 2,300 diversity and need-based scholarships to attend KubeCon + CloudNativeCon and other CNCF hosted events over the course of its life.&nbsp;</p>
-
-
-
 		<div style="height:100px" aria-hidden="true" class="wp-block-spacer">
 		</div>
-
-
-
 		<h2 class="is-style-divider-line" id="2021">Looking Forward to
 			2021<em><br></em></h2>
-
-
-
 		<p
 			class="is-style-max-width-900 has-header-3-font-size">CNCF remains the fastest-growing foundation in the history of open source. The success of CNCF is directly attributable to our projects, the contributions of the community, our end users, and support from our member companies. Thank you!&nbsp;</p>
-
-
-
 		<p
 			class="is-style-default">As we look to 2021 and beyond, we remain committed to fostering and sustaining an ecosystem<strong> </strong>of open source, vendor-neutral projects, and to making technology accessible for everyone. To thrive, we believe CNCF must continue to provide a neutral home for projects, encourage diversity, and continue to cultivate community. All three are crucial elements for growth.</p>
-
-
-
 		<p
 			class="is-style-default">By continuing a wide array of initiatives, such as security audits, project journey reports, and documentation improvements, we are investing in the community to strengthen the cloud native ecosystem. We will continue with our core strategy of focusing on the developer community, helping developers progress into contributor and maintainer roles, and offering educational opportunities for those looking to grow and learn.</p>
-
-
-
 		<p
 			class="is-style-default">Previously, in-person learning opportunities were offered at our flagship KubeCon + CloudNativeCon events. We are now aggressively expanding these learning opportunities through virtual and hybrid events. We plan to expand our events, especially community ones,both initiatives in 2021 to continue supporting community learning.</p>
-
-
-
 		<p
 			class="is-style-default">Our sincere hope is that the CNCF community will continue to evolve as more and more new people join the ecosystem, thus furthering diversity and collaboration. We see signs of this as first-time attendees made up 69% of KubeCon + CloudNativeCon North America Virtual 2020. In 2021, we will also be launching a member referral program that will help support the growth and diversity of our community.</p>
-
-
-
 		<p
 			class="is-style-default">We also will continue to emphasize the growth of our end user community, with new initiatives such as the CNCF End User Technology Radar sharing the real experiences, feedback, and opinions of end users. We will continue our work to bring end users together to discuss best practices in the Telco, Research, and Financial Services sectors.</p>
-
-
-
 		<p
 			class="is-style-max-width-800 has-header-4-font-size">In summary, 2020 was an exceptional year for CNCF. We are well-positioned financially and organizationally to continue our mission to make cloud native computing ubiquitous and the de facto standard for software development and usage. We look forward to having you join us on this journey as we look to 2021 and beyond. </p>
-
-
-
 		<p
 			class="is-style-max-width-800 has-header-3-font-size">We are working diligently to iterate on our strategy for 2021. For the latest updates, please <a href="/blog">see the CNCF blog</a>.</p>
 	</article>
 </main>
-
 <?php
-
 wp_enqueue_script(
-	'annual-report-js',
-	get_stylesheet_directory_uri() . '/source/js/third-party/annual-report.js',
+	'annual-report-21',
+	get_stylesheet_directory_uri() . '/source/js/third-party/annual-report-21.js',
 	array( 'jquery' ),
-	filemtime( get_template_directory() . '/source/js/third-party/annual-report.js' ),
+	filemtime( get_template_directory() . '/source/js/third-party/annual-report-21.js' ),
 	true
 );
-
 get_template_part( 'components/footer' );
