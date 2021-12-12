f7d817bb0829eae79bce599d9ea2428c3768bb89
James Hunt <10615884+thetwopct@users.noreply.github.com>
2021-12-12 22:13:32 +0700

    Updating wp-config for non-Pathenon WP install

--- web/wp-config.php
+++ web/wp-config.php
@@ -23,7 +23,7 @@ $root_path = realpath( __DIR__ . '/..' );
 require_once $root_path . '/vendor/autoload.php';
 
 /*
- * Fetch .env
+ * Fetch .env if exists.
  */
 if ( ! isset( $_ENV['PANTHEON_ENVIRONMENT'] ) && file_exists( $root_path . '/.env' ) ) {
 	$dotenv = Dotenv\Dotenv::create( $root_path );
@@ -54,38 +54,46 @@ define( 'FORCE_SSL_ADMIN', true );
 define( 'WP_POST_REVISIONS', 10 );
 
 /*
- * If NOT on Pantheon / Lando (which is?)
+ * If NOT running on Pantheon or as Pantheon recipe in Lando.
  */
 if ( ! isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ) :
-	/**
-	 * Define site and home URLs
-	 */
-	// HTTP is still the default scheme for now.
-	$scheme = 'http';
-	// If we have detected that the end use is HTTPS, make sure we pass that
-	// through here, so <img> tags and the like don't generate mixed-mode
-	// content warnings.
-	if ( isset( $_SERVER['HTTP_USER_AGENT_HTTPS'] ) && 'ON' == $_SERVER['HTTP_USER_AGENT_HTTPS'] ) {
-		$scheme = 'https';
-	}
 
-	if ( isset( $_SERVER['HTTP_HOST'] ) ) {
-		$site_url = getenv( 'WP_HOME' ) !== false ? getenv( 'WP_HOME' ) : $scheme . '://' . wc_clean( wp_unslash( $_SERVER['HTTP_HOST'] ) ) . '/';
-	}
+	/** This will ensure these are only loaded on Lando */
+	if (getenv('LANDO_INFO')) {
+	/**  Parse the LANDO INFO  */
+	$lando_info = json_decode(getenv('LANDO_INFO'));
 
-	define( 'WP_HOME', $site_url );
-	define( 'WP_SITEURL', $site_url . 'wp/' );
+	/** Get the database config */
+	$database_config = $lando_info->database;
+	/** The name of the database for WordPress */
+	define('DB_NAME', $database_config->creds->database);
+	/** MySQL database username */
+	define('DB_USER', $database_config->creds->user);
+	/** MySQL database password */
+	define('DB_PASSWORD', $database_config->creds->password);
+	/** MySQL hostname */
+	define('DB_HOST', $database_config->internal_connection->host);
 
-	/**
-	 * Set Database Details
-	 */
-	define( 'DB_NAME', getenv( 'DB_NAME' ) );
-	define( 'DB_USER', getenv( 'DB_USER' ) );
-	define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ) !== false ? getenv( 'DB_PASSWORD' ) : '' );
-	define( 'DB_HOST', getenv( 'DB_HOST' ) );
+	/** URL routing */
+	define('WP_HOME','https://cncfci.lndo.site');
+	define('WP_CONTENT_DIR','/app/web/wp-content');
+	define('WP_CONTENT_URL','https://cncfci.lndo.site/wp-content');
 
-	define( 'IS_LOCAL', getenv( 'IS_LOCAL' ) !== false ? true : false );
+  } else {
+		/** The name of the database for WordPress */
+	define( 'DB_NAME', 'wordpress' );
+	/** MySQL database username */
+	define( 'DB_USER', 'wordpress' );
+	/** MySQL database password */
+	define( 'DB_PASSWORD', 'wordpress' );
+	/** MySQL hostname */
+	define( 'DB_HOST', 'databasess' );
+  }
+  /** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
 
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
 	/**#@+
 	 * Authentication Unique Keys and Salts.
 	 *
@@ -103,6 +111,22 @@ if ( ! isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ) :
 	define( 'SECURE_AUTH_SALT', 'pi-EA,AOXk*U[VZ|t]R;@K<WMcbD)>k* ;8+hKX:A|$.Z@HL@0`SE?W0:-?-IRd!' );
 	define( 'LOGGED_IN_SALT', 'e+6%u)u@RZn-$}_Q[N;Na<|A-[Am_$#nhD~}ci:%R&B*oiq<sPF$v)d1r<-V-5W|' );
 	define( 'NONCE_SALT', 'r%oyx_`[A-~<LB)]I.,^//}/&]a)H|fzk3IUWrZn[L4qf#Pp#lsB-B}+/ai&u,/|' );
+
+	// If Lando is running, presume you are local and want debug.
+	if (isset( $_ENV['LANDO'] ) && $_ENV['LANDO'] == 'ON' ) {
+
+		define( 'WP_DEBUG', true );
+		define( 'WP_DEBUG_DISPLAY', true ); // false to go to log.
+		define( 'WP_DEBUG_LOG', __DIR__ . '/wp-content/debug.log' ); // Moves log file to writable location.
+		define( 'SCRIPT_DEBUG', true );
+		define( 'WP_DISABLE_FATAL_ERROR_HANDLER', true ); // stops admin email sent.
+		define( 'BE_MEDIA_FROM_PRODUCTION_URL', 'https://www.cncf.io' );
+
+		// fixes small problem with LH-HSTS
+		if ( ! isset( $_SERVER['HTTP_HOST'] ) ) {
+			$_SERVER['HTTP_HOST'] = 'localhost';
+		}
+	}
 
 endif;
 
@@ -152,6 +176,12 @@ if ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ) :
 	define( 'NONCE_SALT', $_ENV['NONCE_SALT'] );
 	/**#@-*/
 
+	/*
+* Define wp-content directory outside of WordPress core directory.
+*/
+define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/wp-content' );
+define( 'WP_CONTENT_URL', WP_HOME . '/wp-content' );
+
 	/** A couple extra tweaks to help things run well on Pantheon. */
 	if ( isset( $_SERVER['HTTP_HOST'] ) ) {
 		// HTTP is still the default scheme for now.
@@ -195,11 +225,7 @@ if ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ) :
 
 endif;
 
-/*
-* Define wp-content directory outside of WordPress core directory.
-*/
-define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/wp-content' );
-define( 'WP_CONTENT_URL', WP_HOME . '/wp-content' );
+
 
 /**
  * WordPress Database Table prefix.
