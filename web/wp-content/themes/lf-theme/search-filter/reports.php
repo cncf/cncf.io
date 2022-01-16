c2d0ad34a7725829e9958981dc8ec23b2ca8466a
cjyabraham <cjyabraham@gmail.com>
2022-01-16 17:58:29 +0700

    got the reports listed

--- web/wp-content/themes/lf-theme/search-filter/reports.php
+++ web/wp-content/themes/lf-theme/search-filter/reports.php
@@ -24,12 +24,53 @@ if ( $query->have_posts() ) : ?>
 	}
 	?>
 </p>
-<div class="webinars-wrapper">
 
 	<?php
+	$y = 0;
 	while ( $query->have_posts() ) :
 		$query->the_post();
 		$report_type = Lf_Utils::get_term_names( get_the_ID(), 'lf-report-type', true );
+		$report_type_slug = Lf_Utils::get_term_slugs( get_the_ID(), 'lf-report-type', true );
+		$report_year = get_post_meta( get_the_ID(), 'lf_report_published_year', true );
+
+		if ( $y != $report_year ) {
+			if ( 0 != $y ) {
+				echo '</div>';
+			}
+			echo '<h3>' . esc_html( $report_year ) . '</h3>';
+			echo '<div class="webinars-wrapper">';
+			$y = $report_year;
+		}
+		?>
+		<div class="webinar-recorded-box box-shadow">
+
+		<figure>
+			<a href="<?php the_permalink(); ?>">
+				<?php
+				if ( has_post_thumbnail() ) {
+					the_post_thumbnail();
+				} elseif ( isset( $options['generic_thumb_id'] ) && $options['generic_thumb_id'] ) {
+					echo wp_get_attachment_image( $options['generic_thumb_id'], 'full', false, array( 'class' => 'webinar-default' ) );
+				} else {
+					echo '<img src="' . esc_url( get_stylesheet_directory_uri() )
+					. '/images/thumbnail-default.svg" alt="CNCF" class="webinar-default"/>';
+				}
+				?>
+			</a>
+		</figure>
+
+		<?php
+		if ( $report_type ) :
+			$report_type_link = '?_sft_lf-report-type=' . $report_type_slug . '';
+			?>
+			<a class="skew-box secondary" title="See more <?php echo esc_attr( $report_type ); ?> reports" href="<?php echo esc_url( $report_type_link ); ?>">
+			<?php echo esc_html( $report_type ); ?> report</a>
+		<?php endif; ?>
+		<h5 class="webinar-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+
+		</div>
+
+		<?php
 
 	endwhile;
 	?>
