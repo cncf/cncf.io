41202321341dfcac0442e9a6b41a89ae572f1549
cjyabraham <cjyabraham@gmail.com>
2022-01-30 11:44:02 +0700

    allow for type juggling in count comparison

--- web/wp-content/themes/lf-theme/search-filter/reports.php
+++ web/wp-content/themes/lf-theme/search-filter/reports.php
@@ -16,7 +16,7 @@ if ( $query->have_posts() ) : ?>
 	$full_count = $wpdb->get_var( "select count(*) from wp_posts where wp_posts.post_type = 'lf_report' and wp_posts.post_status = 'publish';" );
 
 	// if filter matches all webinars.
-	if ( $full_count === $query->found_posts ) {
+	if ( $full_count == $query->found_posts ) {
 		echo 'Found ' . esc_html( $query->found_posts ) . ' reports';
 	} else {
 		// else show partial count.
