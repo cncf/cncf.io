caadc9c30ce86f182b5165b4e486ad2dc2964424
cjyabraham <cjyabraham@gmail.com>
2022-04-18 21:31:02 +0700

    space out footer links to match figma

--- web/wp-content/themes/cncf-twenty-two/components/copyright.php
+++ web/wp-content/themes/cncf-twenty-two/components/copyright.php
@@ -23,7 +23,7 @@ $site_options = get_option( 'lf-mu' );
 		class="footer__issue">
 		<?php if ( isset( $site_options['accessibility_cta_text'] ) && isset( $site_options['accessibility_cta_link'] ) ) : ?>
 
-					<a href="<?php echo esc_url( get_permalink( $site_options['accessibility_cta_link'] ) ); ?>"><?php echo esc_html( $site_options['accessibility_cta_text'] ); ?></a><br><br>
+					<a href="<?php echo esc_url( get_permalink( $site_options['accessibility_cta_link'] ) ); ?>"><?php echo esc_html( $site_options['accessibility_cta_text'] ); ?></a><br>
 		<?php endif; ?>
 		<a href="https://github.com/cncf/cncf.io/issues/new?title=Your+issue&projects=cncf/cncf.io/1&body=From+URL%3A+<?php echo esc_html( rawurlencode( get_permalink() ) ); ?>">Submit an issue with this page</a>
 	</p>
