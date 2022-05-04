c332ed2de265961619fa06141c9a4cce3a1e7897
James Hunt <10615884+thetwopct@users.noreply.github.com>
2022-05-04 17:28:52 +0700

    Fix parent category spacing on some post types

--- web/wp-content/themes/cncf-twenty-two/components/title-links.php
+++ web/wp-content/themes/cncf-twenty-two/components/title-links.php
@@ -11,16 +11,16 @@
 
 if ( is_singular( 'lf_webinar' ) ) :
 	?>
-<span>
+<div class="parent-link-align">
 	<a class="parent-link"
 		href="<?php echo esc_url( get_home_url() ); ?>/online-programs/"
 		title="Go to online programs">Online program</a>
-</span>
+</div>
 
 	<?php
 elseif ( is_singular( 'lf_spotlight' ) ) :
 	?>
-		<div class="parent-link-align">
+<div class="parent-link-align">
 	<a class="parent-link"
 		href="<?php echo esc_url( get_home_url() ); ?>/spotlights/"
 		title="Go to Spotlights">Spotlight</a>
@@ -31,18 +31,18 @@ elseif ( is_singular( 'lf_spotlight' ) ) :
 	$spotlight_type_link = '/spotlights/?_sft_lf-spotlight-type=' . $spotlight_type_slug;
 
 	?>
-<div class="space-slash">&nbsp;/&nbsp;</div>
-<span><a class="author-category"
-		title="See <?php echo esc_attr( $spotlight_type ); ?> posts"
-		href="<?php echo esc_url( $spotlight_type_link ); ?>">
-		<?php echo esc_html( $spotlight_type ); ?>
-	</a></span>
+	<div class="space-slash">&nbsp;/&nbsp;</div>
+	<span><a class="author-category"
+			title="See <?php echo esc_attr( $spotlight_type ); ?> posts"
+			href="<?php echo esc_url( $spotlight_type_link ); ?>">
+			<?php echo esc_html( $spotlight_type ); ?>
+		</a></span>
 </div>
 	<?php
-	elseif ( is_archive() || is_search() || is_category() ) :
-		?>
-	<!-- display nothing -->
-		<?php
+elseif ( is_archive() || is_search() || is_category() ) :
+	?>
+<!-- display nothing -->
+	<?php
 elseif ( is_tag() || is_tax() ) :
 	// Single Post.
 	$all_categories = get_the_category();
@@ -51,11 +51,11 @@ elseif ( is_tag() || is_tax() ) :
 		// Only get the first item in the array.
 		$category = array_shift( $all_categories );
 		?>
-<span>
+<div class="parent-link-align">
 	<a class="parent-link"
 		href="<?php echo esc_url( get_category_link( $category->term_id ) ); ?>"
 		title="See <?php echo esc_html( $category->name ); ?> posts"><?php echo esc_html( $category->name ); ?></a>
-</span>
+</div>
 		<?php
 	};
 	?>
@@ -64,28 +64,31 @@ elseif ( is_singular( 'lf_project' ) ) :
 	// Check for Sandbox Projects page.
 	if ( get_the_ID() && ( Lf_Utils::get_term_names( get_the_ID(), 'lf-project-stage', true ) == 'Sandbox' ) ) {
 		?>
-<span><a class="parent-link"
+<div class="parent-link-align">
+	<a class="parent-link"
 		href="<?php echo esc_url( get_home_url() ); ?>/sandbox-projects/"
-		title="Go to Sandbox Projects">Projects</a></span>
+		title="Go to Sandbox Projects">Projects</a>
+</div>
 		<?php
 	} else {
 		?>
-<span>
+<div class="parent-link-align">
 	<a class="parent-link"
 		href="<?php echo esc_url( get_home_url() ); ?>/projects/"
 		title="Go to Graduated and Incubating Projects">Projects</a>
-</span>
+</div>
 		<?php
 	}
 elseif ( is_singular( 'lf_kubeweekly' ) ) :
 	?>
-<span><a class="parent-link"
+<div class="parent-link-align"><a class="parent-link"
 		href="<?php echo esc_url( get_home_url() ); ?>/kubeweekly/"
-		title="See all Kubeweeklys">Kubeweekly</a></span>
-		<?php
+		title="See all Kubeweeklys">Kubeweekly</a>
+</div>
+	<?php
 elseif ( is_singular( 'lf_report' ) ) :
 	?>
-		<div class="parent-link-align">
+<div class="parent-link-align">
 	<a class="parent-link"
 		href="<?php echo esc_url( get_home_url() ); ?>/reports/"
 		title="See all Reports">Report</a>
@@ -99,15 +102,15 @@ elseif ( is_singular( 'lf_report' ) ) :
 	if ( $report_type && $report_type_slug ) {
 		$report_type_link = get_home_url() . '/reports/?_sft_lf-report-type=' . $report_type_slug;
 		?>
-<div class="space-slash">&nbsp;/&nbsp;</div>
-<a class="parent-link"
-	title="See more <?php echo esc_attr( $report_type ); ?> reports"
-	href="<?php echo esc_url( $report_type_link ); ?>">
+	<div class="space-slash">&nbsp;/&nbsp;</div>
+	<a class="parent-link"
+		title="See more <?php echo esc_attr( $report_type ); ?> reports"
+		href="<?php echo esc_url( $report_type_link ); ?>">
 		<?php echo esc_html( $report_type ); ?></a>
 		<?php
 	}
 	?>
-	</div>
+</div>
 	<?php
 else :
 	// Single Post.
@@ -116,7 +119,7 @@ else :
 		// Only get the first item in the array.
 		$category = array_shift( $all_categories );
 		?>
-	<div class="parent-link-align">
+<div class="parent-link-align">
 	<a class="parent-link"
 		href="<?php echo esc_url( get_category_link( $category->term_id ) ); ?>"
 		title="See <?php echo esc_html( $category->name ); ?> posts"><?php echo esc_html( $category->name ); ?></a>
@@ -128,15 +131,15 @@ else :
 	if ( $category_author ) {
 		$category_link = get_home_url() . '/lf-author-category/' . $category_author_slug . '/';
 		?>
-<div class="space-slash">&nbsp;/&nbsp;</div>
-<span><a class="author-category"
-		title="See <?php echo esc_attr( $category_author ); ?> posts"
-		href="<?php echo esc_url( $category_link ); ?>">
-		<?php echo esc_html( $category_author ) . ' Post'; ?>
-	</a></span>
+	<div class="space-slash">&nbsp;/&nbsp;</div>
+	<span><a class="author-category"
+			title="See <?php echo esc_attr( $category_author ); ?> posts"
+			href="<?php echo esc_url( $category_link ); ?>">
+			<?php echo esc_html( $category_author ) . ' Post'; ?>
+		</a></span>
 		<?php
 	}
 	?>
-	</div>
+</div>
 	<?php
 endif;
