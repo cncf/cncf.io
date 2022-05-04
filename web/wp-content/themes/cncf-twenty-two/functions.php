9b97625f760ddbf3609957a2f75a33c435402ea9
cjyabraham <cjyabraham@gmail.com>
2022-05-04 17:07:03 +0700

    don't defer jquery (again) as there doesn't seem to be any other way to fix the problem with the search/filter dropdowns not appearing; plugin author couldn't help

--- web/wp-content/themes/cncf-twenty-two/functions.php
+++ web/wp-content/themes/cncf-twenty-two/functions.php
@@ -129,7 +129,7 @@ function lf_defer_parsing_of_js( $url ) {
 		return $url;
 	}
 	// List of scripts that should not be deferred.
-	$do_not_defer_scripts = array();
+	$do_not_defer_scripts = array( 'jquery' );
 	if ( count($do_not_defer_scripts) > 0 ) {
  	foreach( $do_not_defer_scripts as $script ){
 		if ( strpos( $url, $script ) ) {
