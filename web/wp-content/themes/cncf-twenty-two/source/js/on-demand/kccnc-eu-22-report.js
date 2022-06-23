bf646a51461eafdf24094574b557176d74a1d906
James Hunt <10615884+thetwopct@users.noreply.github.com>
2022-06-24 00:29:29 +0700

    #563 Adding Photo Slider
    Signed-off-by: James Hunt <10615884+thetwopct@users.noreply.github.com>

--- web/wp-content/themes/cncf-twenty-two/source/js/on-demand/kccnc-eu-22-report.js
+++ web/wp-content/themes/cncf-twenty-two/source/js/on-demand/kccnc-eu-22-report.js
@@ -1,32 +1,65 @@
-/** // phpcs:ignoreFile
+/**
  * KCCNC EU 2022 JS
  *
  * @package WordPress
  * @since 1.0.0
  */
 
- jQuery( document ).ready(
+jQuery( document ).ready(
 	function( $ ) {
+		toggleJobs( $ );
+		photoSlider( $ );
 
-	const open = $('*[data-id="js-hidden-section-trigger-open"]');
-	const close = $('*[data-id="js-hidden-section-trigger-close"]');
-	const section = $('*[data-id="js-hidden-section"]');
+	}
+);
 
-	if (! open || ! section ) {
+function photoSlider($) {
+
+	const slider = $( '.section-02__slider' );
+
+	if ( ! slider ) {
 		return;
 	}
 
-	open.click( function(){
-		open.slideToggle(100);
-		section.slideToggle(600);
-		close.slideToggle(300);
-	});
+	slider.slick(
+		{
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			swipeToSlide: true,
+			draggable: true,
+			centerMode: false,
+			autoplay: true,
+			appendArrows: false,
+			arrows: true,
+			prevArrow: $( '.section-02__prev' ),
+			nextArrow: $( '.section-02__next' )
+		}
+	);
+}
 
-	close.click( function(){
-		open.slideToggle(100);
-		section.slideToggle(600);
-		close.slideToggle(300);
-	});
+function toggleJobs($){
+	const open    = $( '*[data-id="js-hidden-section-trigger-open"]' );
+	const close   = $( '*[data-id="js-hidden-section-trigger-close"]' );
+	const section = $( '*[data-id="js-hidden-section"]' );
+
+	if ( ! open || ! section ) {
+		return;
+	}
+
+	open.click(
+		function(){
+			open.slideToggle( 100 );
+			section.slideToggle( 600 );
+			close.slideToggle( 300 );
+		}
+	);
+
+	close.click(
+		function(){
+			open.slideToggle( 100 );
+			section.slideToggle( 600 );
+			close.slideToggle( 300 );
+		}
+	);
 
 }
-);
