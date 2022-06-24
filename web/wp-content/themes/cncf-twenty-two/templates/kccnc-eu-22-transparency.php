a51e17020da761ed40244f1bfb35332b13e0692d
James Hunt <10615884+thetwopct@users.noreply.github.com>
2022-06-24 15:18:25 +0700

    #546 Updating embdeed video thumbnail to be SD
    Signed-off-by: James Hunt <10615884+thetwopct@users.noreply.github.com>

--- web/wp-content/themes/cncf-twenty-two/templates/kccnc-eu-22-transparency.php
+++ web/wp-content/themes/cncf-twenty-two/templates/kccnc-eu-22-transparency.php
@@ -2549,8 +2549,8 @@ https://www.techtarget.com/searchitoperations/news/252518485/Cisco-CNCF-leader-u
 					<div aria-hidden="true" class="report-spacer-60"></div>
 
 					<div class="wp-block-lf-youtube-lite">
-						<lite-youtube videoid="SqesB4xcAUY" webpStatus="1"
-							sdthumbStatus="0">
+						<lite-youtube videoid="SqesB4xcAUY" webpStatus="0"
+							sdthumbStatus="1">
 						</lite-youtube>
 					</div>
 				</div>
