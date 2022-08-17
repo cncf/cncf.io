193ad96dafcd7f18a059f3d93c0b269745f6541d
James Hunt <10615884+thetwopct@users.noreply.github.com>
2022-08-17 23:32:30 +0700

    CTO Summit report
    Signed-off-by: James Hunt <10615884+thetwopct@users.noreply.github.com>

---
+++ web/wp-content/themes/cncf-twenty-two/source/js/on-demand/cto-summit-eu-22-report.js
@@ -0,0 +1,37 @@
+/**
+ * CTO Summit EU 2022 JS
+ *
+ * @package WordPress
+ * @since 1.0.0
+ */
+
+jQuery( document ).ready(
+	function( $ ) {
+		photoSlider( $ );
+
+	}
+);
+
+function photoSlider($) {
+
+	const slider = $( '.section-10__slider' );
+
+	if ( ! slider ) {
+		return;
+	}
+
+	slider.slick(
+		{
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			swipeToSlide: true,
+			draggable: true,
+			centerMode: false,
+			autoplay: true,
+			appendArrows: false,
+			arrows: true,
+			prevArrow: $( '.section-10__prev' ),
+			nextArrow: $( '.section-10__next' )
+		}
+	);
+}
