3582833acab6a369b523693322ed62054c84b9c1
James Hunt <10615884+thetwopct@users.noreply.github.com>
2022-09-07 00:05:19 +0700

    Updating Tab Container
    Signed-off-by: James Hunt <10615884+thetwopct@users.noreply.github.com>

--- web/wp-content/themes/cncf-twenty-two/source/js/on-demand/tab-container.js
+++ web/wp-content/themes/cncf-twenty-two/source/js/on-demand/tab-container.js
@@ -5,16 +5,13 @@
  * @since 1.0.0
  */
 
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
-/* eslint-disable array-callback-return */
-/* eslint-disable no-var */
-
 jQuery( document ).ready(
 	function( $ ) {
-		// Setup Sticky.
-		let setSticky;
-		let stickyMenu = $( '.sticky__nav' );
+
+		// Get all the links in the sticky nav.
+		let menuItems = $( '.sticky__nav' ).find( 'a' );
+		// Set disableScroll.
+		let disableScroll = false;
 		// Set default animation speed.
 		let animationSpeed = 500;
 		// Get matchMedia setting.
@@ -22,31 +19,36 @@ jQuery( document ).ready(
 		let prefersReducedMotionQuery   = window.matchMedia( '(prefers-reduced-motion: reduce)' );
 		let prefersReducedMotion        = ! prefersReducedMotionQuery || prefersReducedMotionQuery.matches;
 
+		if ( $( '.sticky__nav' ).length == 0 ) {
+			return;
+		}
+
 		/**
-		 * Apply Sticky
-		 */
+		* Sticky Menu.
+		*/
+		let setSticky;
 		( setSticky = function() {
 			sticky = new Sticky(
 				'.sticky__element',
 				{
 					marginTop: getSpacing(),
-					marginBottom: 100,
+					marginBottom: 0,
 					stickyFor: 800,
 				}
 			);
 		} )();
 
 		/**
-		 Sets animation speed based on preferences.
+		 * Set Animation Speed based on Prefers Motion.
 		 */
-		function getMotionMatch() {
+		function getAnimationSpeed() {
 			if (prefersReducedMotion) {
 				animationSpeed = 0
 			} else {
 				animationSpeed = 500
 			}
 		}
-		getMotionMatch();
+		getAnimationSpeed();
 
 		/**
 		 * Get spacing required from top of window for content.
@@ -62,9 +64,9 @@ jQuery( document ).ready(
 			if ( winH < 616 && winW > 514 ) {
 				spacingTotal += 40;
 			} else if ( winW < 800 ) {
-				spacingTotal += 80;
+				spacingTotal += 100;
 			} else {
-				spacingTotal += 125;
+				spacingTotal += 140;
 			}
 			if ( adminBar.length > 0 ) {
 				spacingTotal += 32;
@@ -72,104 +74,127 @@ jQuery( document ).ready(
 			return spacingTotal;
 		}
 
-		if ( stickyMenu.length > 0 ) {
-			// Get all the links in the sticky nav.
-			let menuItems = stickyMenu.find( 'a' );
-			// Use the links to confirm matching page sections.
-			let pageSections = menuItems.map(
-				function() {
-					let item = $( $( this ).attr( 'href' ) );
-					if ( item.length ) {
-						return item;
-					}
+		/**
+		 * Use menu items to confirm matching page sections.
+		 */
+		let pageSections = menuItems.map(
+			function() {
+				let item = $( $( this ).attr( 'href' ) );
+				if ( item.length ) {
+					return item;
 				}
-			);
+			}
+		);
 
 			/**
-			 * Listen for clicks on menu items then fancy scroll to page section.
+			 * Watch clicks to scroll smoothly to each.
 			 */
-			menuItems.click(
-				function( e ) {
-					e.preventDefault();
-					let href      = $( this ).attr( 'href' );
-					let offsetTop = href === '#' ? 0 : $( href ).offset().top - getSpacing();
-					$( 'html, body' )
-					.stop()
-					.animate(
-						{
-							scrollTop: offsetTop,
-						},
-						animationSpeed
-					);
-					menuItems
-						.parent()
-						.removeClass( 'is-active' );
-					$( this ).parent().addClass( 'is-active' );
-				}
-			);
-
-			/**
-			 * Update Nav, Hash as user scrolls.
-			 */
-			function navUpdate() {
-				let fromTop    = $( this ).scrollTop();
-				let headerSize = getSpacing();
-				// Get all the page sections passed by scrolling.
-				let currentItemElements = pageSections.map(
+		menuItems.click(
+			function( e ) {
+				e.preventDefault();
+				disableScroll = true;
+				let hash      = $( this ).attr( 'href' );
+				let offsetTop = hash === '#' ? 0 : $( hash ).offset().top - getSpacing();
+				$( 'html, body' )
+				.stop()
+				.animate(
+					{
+						scrollTop: offsetTop,
+					},
+					animationSpeed,
 					function() {
-						// If true, return the section name(s).
-						if ( $( this ).offset().top < fromTop + headerSize ) {
-							// console.log($( this ).offset().top)
-							// console.log(fromTop + headerSize)
-							return this;
-						}
+						removeIsActiveFromMenu();
+						$( '.sticky__nav a[href="#' + $( hash ).attr( 'id' ) + '"]' ).parent().addClass( 'is-active' );
+						setHash( $( hash ) );
+						disableScroll = false;
 					}
 				);
+			}
+		);
 
-				// Get the ID of the last section seen.
-				let currentItemId = currentItemElements[ currentItemElements.length - 1 ];
+		/**
+		 * Remove Is Active Class from all items in menu.
+		 */
+		function removeIsActiveFromMenu(){
+			menuItems.parent().removeClass( 'is-active' );
+		}
 
-				// if neither exist we're outside sections so removeHash.
-				if (currentItemElements.length == 0 && ! currentItemId ) {
-					removeHash();
-					menuItems
-						.parent()
-						.removeClass( 'is-active' )
-				}
-
-				if ( ! currentItemId ) {
-					return;
-				}
-
-				// Get the precise anchor ID to use.
-				let id = currentItemId && currentItemId.length ? currentItemId[ 0 ].id : '';
-
-				if ( id  ) {
-						// Set/remove active class.
-						menuItems
-						.parent()
-						.removeClass( 'is-active' )
-						.end()
-						.filter( "[href='#" + id + "']" )
-						.parent()
-						.addClass( 'is-active' );
-
-						// Set hash.
-					if ( history.pushState ) {
-						window.history.replaceState(
-							null,
-							null,
-							'#' + id
-						);
-					} else {
-						// IE9, IE8, etc.
-						window.location.hash = '#!' + id;
+		/**
+		 * Update the menu as the page scrolls.
+		 */
+		function navUpdate() {
+			if (disableScroll == true) {
+				return;
+			}
+			let fromTop    = $( this ).scrollTop();
+			let headerSize = getSpacing();
+			// Get all the page sections passed by scrolling.
+			let currentItemElements = pageSections.map(
+				function() {
+					// If true, return the section name(s).
+					if ( $( this ).offset().top < fromTop + headerSize ) {
+						return this;
 					}
 				}
+			);
+
+			// Get the ID of the last section seen.
+			let currentItemId = currentItemElements[ currentItemElements.length - 1 ];
+
+			// if neither exist we're outside sections so removeHash.
+			if (currentItemElements.length == 0 && ! currentItemId ) {
+				removeHash();
+				removeIsActiveFromMenu();
+			}
+
+			if ( ! currentItemId ) {
+				return;
+			}
+
+			// Get the precise anchor ID to use.
+			let id = currentItemId && currentItemId.length ? currentItemId[ 0 ].id : '';
+
+			if ( id  ) {
+				removeIsActiveFromMenu();
+				menuItems.filter( "[href='#" + id + "']" )
+				.parent()
+				.addClass( 'is-active' )
+				makeNavInView();
+				setHash( currentItemId );
+
 			}
 		}
 
-		// Remove hash from URL.
+		/**
+		 * Sets the URL hash
+		 *
+		 * @param {object} object
+		 * @returns
+		 */
+		function setHash(object){
+			let id = object && object.length ? object[ 0 ].id : '';
+
+			// if no id, or the current hash is the same as the id, do nothing.
+			if ( ! id || '#' + id === window.location.hash ) {
+				return;
+			}
+
+			// Set hash.
+			if ( history.pushState ) {
+				window.history.replaceState(
+					null,
+					null,
+					'#' + id
+				);
+			} else {
+				// For older browsers, IE9, IE8, etc.
+				window.location.hash = '#!' + id;
+			}
+		}
+
+		/**
+		 * Removes the URL hash.
+		 */
 		function removeHash() {
 			let scrollV;
 			let scrollH;
@@ -188,53 +213,62 @@ jQuery( document ).ready(
 			}
 		}
 
-		// Looks for nav item and checks its in view.
-		// TODO: This fucks up initial scrolling. Should only activate if the nav is sticky!!!
-		function navInView() {
-
+		/**
+		 * Make the navigation scroll in to view.
+		 */
+		function makeNavInView() {
 			let currentItem = $( '.sticky__nav-item.is-active' );
 			let winW        = $( window ).width();
-
 			if ( winW > 799 && currentItem.length > 0 ) {
-				console.log( 'Update scroll in to view' )
 				currentItem[ 0 ].scrollIntoView(
 					{
-						behavior: 'instant',
-						block: 'end',
-						// inline: 'start'
+						block: 'center',
 					}
 				);
 			}
 		}
 
-		// Check nav items are in view as user scrolls.
-		// $( window ).on( 'scroll', window.utils.isThrottled( navInView, 200, true ) );
-
-		// Update nav and hash as user scrolls.
+		/**
+		 * Update nav and hash as user scrolls.
+		 */
 		$( window ).on( 'scroll', window.utils.isThrottled( navUpdate, 200, true ) );
 
-		// Update Sticky on resize.
-		$( window ).on( 'resize', window.utils.isThrottled( setSticky, 200, true ) );
+		/**
+		 * Update Sticky on resize.
+		 */
+		$( window ).on( 'resize', window.utils.isThrottled( setSticky, 250, true ) );
 
-		// Watches for change event to reload based on prefs.
+		/**
+		 * Watches for change event to reload based on prefs.
+		 */
 		if (prefersReducedMotionSetting.addEventListener) {
-			prefersReducedMotionSetting.addEventListener( 'change', getMotionMatch );
+			prefersReducedMotionSetting.addEventListener( 'change', getAnimationSpeed );
 		}
 
-		// If page loads with hash, go to it nicely after 1s.
+		/**
+		 * If page loads with hash, go to it nicely after 1s.
+		 */
 		if ( window.location.hash ) {
-			// smooth scroll to the anchor id if exists after 1s.
 			if ( $( window.location.hash ).length ) {
-				let theHash   = $( window.location.hash );
-				let offsetNew = theHash === '#' ? 0 : theHash.offset().top - getSpacing();
+				disableScroll           = true;
+				let originalHash        = window.location.hash;
+				let matchingHashElement = $( window.location.hash );
+				let offsetNew           = matchingHashElement === '#' ? 0 : matchingHashElement.offset().top - getSpacing();
 				setTimeout(
 					function() {
 						$( 'html, body' )
+						.stop()
 						.animate(
 							{
 								scrollTop: offsetNew,
 							},
-							animationSpeed
+							animationSpeed,
+							function() {
+								removeIsActiveFromMenu();
+								$( '.sticky__nav a[href="#' + $( originalHash ).attr( 'id' ) + '"]' ).parent().addClass( 'is-active' );
+								makeNavInView();
+								disableScroll = false;
+							}
 						);
 					},
 					1000
