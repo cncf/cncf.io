72c4bdeef335047e1bf83a31b5323fd16be80f8d
cjyabraham <cjyabraham@gmail.com>
2022-11-13 17:09:46 -0500

    set up new shortcode
    Signed-off-by: cjyabraham <cjyabraham@gmail.com>

--- web/wp-content/themes/cncf-twenty-two/includes/shortcodes/project-charts.php
+++ web/wp-content/themes/cncf-twenty-two/includes/shortcodes/project-charts.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Projects Chart Shortcode
+ * Project Chart Shortcodes
  *
  * Usage:
  * [projects stage="graduated|incubating|sandbox"]
@@ -82,7 +82,7 @@ function get_chart_data( $maturity_data ) {
 }
 
 /**
- * Add Projects shortcode.
+ * Add Projects Chart shortcode.
  *
  * @param array $atts Attributes.
  */
@@ -138,3 +138,62 @@ function add_projects_chart_shortcode( $atts ) {
 	return $block_content;
 }
 add_shortcode( 'projects-chart', 'add_projects_chart_shortcode' );
+
+
+/**
+ * Add Projects Accepted Chart shortcode.
+ *
+ * @param array $atts Attributes.
+ */
+function add_projects_accepted_chart_shortcode( $atts ) {
+
+	$maturity_data  = get_maturity_data();
+	$chart_data     = get_chart_data( $maturity_data );
+	$project_months = array_keys( $chart_data );
+	$sandbox        = array();
+	$incubating     = array();
+	$graduated      = array();
+	$archived       = array();
+
+	foreach ( $chart_data as $cd ) {
+		$sandbox[]    = $cd['sandbox'];
+		$incubating[] = $cd['incubating'];
+		$graduated[]  = $cd['graduated'];
+		$archived[]   = $cd['archived'];
+	}
+
+	ob_start();
+
+	// chart js.
+	wp_enqueue_script(
+		'chart-js',
+		get_template_directory_uri() . '/source/js/libraries/chart-3.9.1.min.js',
+		null,
+		filemtime( get_template_directory() . '/source/js/libraries/chart-3.9.1.min.js' ),
+		true
+	);
+	// custom script.
+	wp_enqueue_script(
+		'projects-chart',
+		get_template_directory_uri() . '/source/js/on-demand/projects-chart.js',
+		array( 'jquery', 'chart-js' ),
+		filemtime( get_template_directory() . '/source/js/on-demand/projects-chart.js' ),
+		true
+	);
+	?>
+<div class="projects-chart-container">
+	<canvas id="projectsChart"></canvas>
+</div>
+<script>
+	const project_months     = <?php echo json_encode( $project_months ); ?>;
+	const project_sandbox    = <?php echo json_encode( $sandbox ); ?>;
+	const project_incubating = <?php echo json_encode( $incubating ); ?>;
+	const project_graduated  = <?php echo json_encode( $graduated ); ?>;
+	const project_archived   = <?php echo json_encode( $archived ); ?>;
+</script>
+
+	<?php
+	$block_content = ob_get_clean();
+	return $block_content;
+}
+add_shortcode( 'projects-accepted-chart', 'add_projects_accepted_chart_shortcode' );
