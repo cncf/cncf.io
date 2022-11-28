05e0952f6472ba089c47e6a92f890345f0f77228
cjyabraham <cjyabraham@gmail.com>
2022-11-28 14:35:33 -0800

    clear cache after synching people
    Signed-off-by: cjyabraham <cjyabraham@gmail.com>

--- web/wp-content/mu-plugins/wp-mu-plugins/lf-mu/includes/class-lf-mu-rest-controller.php
+++ web/wp-content/mu-plugins/wp-mu-plugins/lf-mu/includes/class-lf-mu-rest-controller.php
@@ -56,6 +56,16 @@ class LF_MU_REST_Controller extends WP_REST_Controller {
 			if ( 'nextarch' === $json->repository->name && 'closed' === $json->action && true === $json->pull_request->merged ) {
 				// sync people.
 				wp_schedule_single_event( time(), 'lf_sync_people' );
+
+				pantheon_wp_clear_edge_paths(
+					array(
+						'/people/staff',
+						'/people/technical-oversight-committee',
+						'/people/governing-board',
+						'/people/ambassadors'
+					),
+				);
+
 				return new WP_REST_Response( array( 'Success. People synched.' ), 200 );
 			}
 		}
