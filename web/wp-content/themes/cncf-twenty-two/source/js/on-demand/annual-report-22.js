56b885927b1ef175128b605562fa255c732966ec
James Hunt <10615884+thetwopct@users.noreply.github.com>
2022-12-15 22:34:47 +0700

    CNCF Annual Report 2022 (#637)

--- web/wp-content/themes/cncf-twenty-two/source/js/on-demand/annual-report-22.js
+++ web/wp-content/themes/cncf-twenty-two/source/js/on-demand/annual-report-22.js
@@ -5,6 +5,32 @@
  * @since 1.0.0
  */
 
-function ready( fn ) {
+jQuery( document ).ready(
+	function( $ ) {
+		photoSlider( $ );
+	}
+);
 
+function photoSlider($) {
+
+	const slider = $( '.section-02__slider' );
+
+	if ( ! slider ) {
+		return;
+	}
+
+	slider.slick(
+		{
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			swipeToSlide: true,
+			draggable: true,
+			centerMode: false,
+			autoplay: true,
+			appendArrows: false,
+			arrows: true,
+			prevArrow: $( '.section-02__prev' ),
+			nextArrow: $( '.section-02__next' )
+		}
+	);
 }
