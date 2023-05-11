08f94dcf8ae9739827f58dc7124b718ab1ad0474
cjyabraham <cjyabraham@gmail.com>
2023-05-11 20:42:48 +0700

    update cluster styles
    Signed-off-by: cjyabraham <cjyabraham@gmail.com>

--- web/wp-content/themes/cncf-twenty-two/source/js/on-demand/people-map.js
+++ web/wp-content/themes/cncf-twenty-two/source/js/on-demand/people-map.js
@@ -37,9 +37,7 @@
 
 			const svg = window.btoa(`
                 <svg fill="#000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 240 240">
-                <circle cx="120" cy="120" opacity="1" r="70" />
-                <circle cx="120" cy="120" opacity=".7" r="90" />
-                <circle cx="120" cy="120" opacity=".3" r="110" />
+                <circle cx="120" cy="120" opacity=".7" r="110" />
                 </svg>`);
 
 			const renderer = {
