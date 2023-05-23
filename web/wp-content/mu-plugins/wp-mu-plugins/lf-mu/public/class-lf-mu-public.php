11a5f408a97c20a26d40c67bb4c7dc8af99a8d32
cjyabraham <cjyabraham@gmail.com>
2023-05-23 17:21:30 +0700

    Only cache in browser for 60s
    Signed-off-by: cjyabraham <cjyabraham@gmail.com>

--- web/wp-content/mu-plugins/wp-mu-plugins/lf-mu/public/class-lf-mu-public.php
+++ web/wp-content/mu-plugins/wp-mu-plugins/lf-mu/public/class-lf-mu-public.php
@@ -329,7 +329,7 @@ class Lf_Mu_Public {
 	 */
 	public function add_header_cache() {
 		if ( ! is_admin() && ! is_user_logged_in() ) {
-			header( 'Cache-Control: public, max-age=600, s-maxage=43200, stale-while-revalidate=86400, stale-if-error=604800' );
+			header( 'Cache-Control: public, max-age=60, s-maxage=43200, stale-while-revalidate=86400, stale-if-error=604800' );
 		}
 	}
 }
