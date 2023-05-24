a8eb48b39c1e2ab0764bb427bfc30bce05135bfe
cjyabraham <cjyabraham@gmail.com>
2023-05-24 13:06:36 +0700

    added copy to the training newsletter form
    Signed-off-by: cjyabraham <cjyabraham@gmail.com>

--- web/wp-content/themes/cncf-twenty-two/includes/shortcodes/training.php
+++ web/wp-content/themes/cncf-twenty-two/includes/shortcodes/training.php
@@ -21,6 +21,8 @@ function add_training_newsletter_shortcode( $atts ) {
 <div class="wp-block-group is-style-box-shadow has-white-background-color has-background kubeweekly-newsletter">
 <h3 class="is-style-spaced-uppercase">JOIN THE TRAINING MAILING LIST</h3>
 <div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
+<p>Stay up-to-date on all things Training + Certification by signing up for our mailing list:</p>
+<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
 	<?php echo do_shortcode( '[hubspot type=form portal=8112310 id=4724da06-1f07-4ee1-af9a-2980a803766b]' ); ?>
 	<div style="height:15px" aria-hidden="true" class="wp-block-spacer is-style-40-responsive"></div>
 <p class="has-small-font-size margin-top">By submitting this form, you acknowledge that your information is subject to The Linux Foundation’s <a href="https://www.linuxfoundation.org/privacy/">Privacy Policy</a>.</p>
