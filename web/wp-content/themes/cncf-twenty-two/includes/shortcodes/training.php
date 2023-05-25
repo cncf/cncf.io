634c93b3f6112444294e364bb76453d5f3b76951
cjyabraham <cjyabraham@gmail.com>
2023-05-25 09:37:07 +0700

    style the updated training newsletter form
    Signed-off-by: cjyabraham <cjyabraham@gmail.com>

--- web/wp-content/themes/cncf-twenty-two/includes/shortcodes/training.php
+++ web/wp-content/themes/cncf-twenty-two/includes/shortcodes/training.php
@@ -24,8 +24,6 @@ function add_training_newsletter_shortcode( $atts ) {
 <p>Stay up-to-date on all things Training + Certification by signing up for our mailing list:</p>
 <div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
 	<?php echo do_shortcode( '[hubspot type=form portal=8112310 id=4724da06-1f07-4ee1-af9a-2980a803766b]' ); ?>
-	<div style="height:15px" aria-hidden="true" class="wp-block-spacer is-style-40-responsive"></div>
-<p class="has-small-font-size margin-top">By submitting this form, you acknowledge that your information is subject to The Linux Foundation’s <a href="https://www.linuxfoundation.org/privacy/">Privacy Policy</a>.</p>
 </div>
 	<?php
 	$block_content = ob_get_clean();
