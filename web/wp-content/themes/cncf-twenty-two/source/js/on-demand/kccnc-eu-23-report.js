3bdcfdcd72960e9e226582fc89c229f35a014527
James Hunt <10615884+thetwopct@users.noreply.github.com>
2023-06-13 02:04:12 +0300

    KCCNC EU 23 Report Draft

---
+++ web/wp-content/themes/cncf-twenty-two/source/js/on-demand/kccnc-eu-23-report.js
@@ -0,0 +1,74 @@
+/**
+ * KCCNC EU 2023 JS
+ *
+ * @package WordPress
+ * @since 1.0.0
+ */
+
+jQuery( document ).ready(
+	function( $ ) {
+		toggleSection( $ );
+		photoSlider( $ );
+		masonry( $ );
+	}
+);
+
+function masonry($) {
+
+	const grid = $( '.masonry-grid' );
+
+	grid.masonry(
+		{
+			itemSelector: '.masonry-item',
+			columnWidth: '.masonry-item',
+			percentPosition: true,
+			gutter: 30
+		}
+	);
+}
+
+function photoSlider($) {
+
+	const slider = $( '.section-02__slider' );
+
+	if ( ! slider ) {
+		return;
+	}
+
+	slider.slick(
+		{
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			swipeToSlide: true,
+			draggable: true,
+			centerMode: false,
+			autoplay: true,
+			appendArrows: false,
+			arrows: true,
+			prevArrow: $( '.section-02__prev' ),
+			nextArrow: $( '.section-02__next' )
+		}
+	);
+}
+function toggleSection($) {
+
+	const buttons = $( '[data-id^="js-hidden-section-trigger"]' );
+
+	buttons.each(
+		function() {
+			const button = $( this );
+			const parentSection = button.closest( '[class^="section-"]' );
+			const open = parentSection.find( '[data-id="js-hidden-section-trigger-open"]' );
+			const close = parentSection.find( '[data-id="js-hidden-section-trigger-close"]' );
+			const section = parentSection.find( '[data-id="js-hidden-section"]' );
+
+			button.click(
+				function() {
+					open.slideToggle( 100 );
+					section.slideToggle( 600 );
+					close.slideToggle( 300 );
+				}
+			);
+		}
+	);
+}
