b6a61d89b5319f1e982a6fe777d9effa5f03c970
James Hunt <10615884+thetwopct@users.noreply.github.com>
2023-07-13 21:23:35 +0100

    Adding width and height to project logos shortcode
    Signed-off-by: James Hunt <10615884+thetwopct@users.noreply.github.com>

--- web/wp-content/themes/cncf-twenty-two/includes/shortcodes/projects.php
+++ web/wp-content/themes/cncf-twenty-two/includes/shortcodes/projects.php
@@ -66,7 +66,7 @@ function add_projects_shortcode( $atts ) {
 			title="<?php echo esc_html( the_title_attribute() . $date_accepted ); ?>"
 			class="project-item__link">
 
-			<img src="<?php echo esc_url( $logo ); ?>" loading="lazy"
+			<img width="100" height="100" src="<?php echo esc_url( $logo ); ?>" loading="lazy"
 				title="<?php echo esc_html( the_title_attribute() . $date_accepted ); ?>"
 				class="project-item__image">
 		</a>
