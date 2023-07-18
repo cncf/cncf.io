fb0ecde9fda2e9aa9770199bb72633af6bb51872
cjyabraham <cjyabraham@gmail.com>
2023-07-18 17:46:12 +0800

    Added play button for programs on projects pages (fixes #759)
    Signed-off-by: cjyabraham <cjyabraham@gmail.com>

--- web/wp-content/themes/cncf-twenty-two/components/project-single.php
+++ web/wp-content/themes/cncf-twenty-two/components/project-single.php
@@ -267,6 +267,16 @@ more recordings</a></p>
 			</div>
 			<div style="height:40px" aria-hidden="true"
 				class="wp-block-spacer is-style-20-40"></div>
+<!-- Embeded svg sprite reference -->
+<svg display="none" xmlns="http://www.w3.org/2000/svg">
+ <symbol id="play-button" fill="none" viewBox="0 0 70 71" id=".5155955424562817" xmlns="http://www.w3.org/2000/svg">
+ <g clip-path="url(#clip0_4409_15889)">
+ <path d="M35 70.468c19.33 0 35-15.67 35-35s-15.67-35-35-35-35 15.67-35 35 15.67 35 35 35z" fill="#D62293"/>
+ <path d="M26.676 51.298V18.964a2.682 2.682 0 0 1 4.394-2.06l19.367 16.177a2.686 2.686 0 0 1 0 4.115L31.07 53.362a2.676 2.676 0 0 1-4.394-2.064z" fill="#fff"/>
+ </g>
+ </symbol>
+</svg>
+
 			<div class="webinars columns-three">
 				<?php
 				while ( $programs_query->have_posts() ) :
