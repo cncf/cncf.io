a5413999ec6ae2add06f97a34796aeaee8f12487
Chris Abraham <cjyabraham@gmail.com>
2023-09-13 12:22:49 -0700

    Expose the basic data points
    Signed-off-by: Chris Abraham <cjyabraham@gmail.com>

--- web/wp-content/themes/cncf-twenty-two/components/project-single.php
+++ web/wp-content/themes/cncf-twenty-two/components/project-single.php
@@ -18,7 +18,9 @@ $description      = get_post_meta( get_the_id(), 'lf_project_description', true 
 $project_category = get_post_meta( get_the_ID(), 'lf_project_category', true );
 $external_url     = get_post_meta( get_the_ID(), 'lf_project_external_url', true );
 
-$date_accepted = get_post_meta( get_the_ID(), 'lf_project_date_accepted', true ) ? gmdate( 'F j, Y', strtotime( get_post_meta( get_the_ID(), 'lf_project_date_accepted', true ) ) ) : '';
+$date_accepted   = get_post_meta( get_the_ID(), 'lf_project_date_accepted', true ) ? gmdate( 'F j, Y', strtotime( get_post_meta( get_the_ID(), 'lf_project_date_accepted', true ) ) ) : '';
+$date_incubating = get_post_meta( get_the_ID(), 'lf_project_date_incubating', true ) ? gmdate( 'F j, Y', strtotime( get_post_meta( get_the_ID(), 'lf_project_date_incubating', true ) ) ) : '';
+$date_graduated  = get_post_meta( get_the_ID(), 'lf_project_date_graduated', true ) ? gmdate( 'F j, Y', strtotime( get_post_meta( get_the_ID(), 'lf_project_date_graduated', true ) ) ) : '';
 
 // Links for Project.
 $github         = get_post_meta( get_the_ID(), 'lf_project_github', true );
@@ -65,12 +67,11 @@ $project_slug = strtolower( get_the_title() );
 				if ( $date_accepted && $stage ) {
 					?>
 				<p class="projects-single-box__accepted">
-					<?php the_title(); ?>&nbsp;was accepted to CNCF on
-<strong><?php echo esc_html( $date_accepted ); ?></strong>
-and is at the
-<strong><?php echo esc_html( $stage ); ?></strong>
-project maturity level.
-</p>
+
+					Project accepted: <strong><?php echo esc_html( $date_accepted ); ?></strong><br />
+					Project incubating: <strong><?php echo esc_html( $date_incubating ); ?></strong><br />
+					Project graduated: <strong><?php echo esc_html( $date_graduated ); ?></strong><br />
+					Current maturity level: <strong><?php echo esc_html( $stage ); ?></strong><br />
 					<?php
 				} elseif ( $stage ) {
 					?>
