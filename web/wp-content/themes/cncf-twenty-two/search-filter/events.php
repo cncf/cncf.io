49cc4527b7be5c6ca66f25f7e90d8d9f6e431214
Chris Abraham <cjyabraham@gmail.com>
2023-11-14 11:16:24 +0700

    fix bug counting events
    Signed-off-by: Chris Abraham <cjyabraham@gmail.com>

--- web/wp-content/themes/cncf-twenty-two/search-filter/events.php
+++ web/wp-content/themes/cncf-twenty-two/search-filter/events.php
@@ -15,7 +15,7 @@ if ( $query->have_posts() ) : ?>
 <p class="search-filter-results-count">
 	<?php
 	$dd         = current_datetime();
-	$full_count = $wpdb->get_var( $wpdb->prepare( "select count(*) from wp_posts join wp_postmeta on wp_posts.ID = wp_postmeta.post_id where wp_posts.post_type = 'lf_event' and wp_posts.post_status = 'publish' and meta_key='lf_event_date_end' and meta_value >= %s;", $dd->format( 'Y-m-d' ) ) );
+	$full_count = $wpdb->get_var( $wpdb->prepare( "select count(*) from wp_posts join wp_postmeta on wp_posts.ID = wp_postmeta.post_id where wp_posts.post_type = 'lf_event' and wp_posts.post_status = 'publish' and meta_key='lf_event_date_end' and meta_value >= %s;", $dd->format( 'Y/m/d' ) ) );
 
 	// if filter matches all.
 	if ( $full_count == $query->found_posts ) {
