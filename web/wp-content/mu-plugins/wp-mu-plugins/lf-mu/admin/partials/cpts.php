7bb3a57d21c9957cc47d75f1646715a8779182f4
Chris Abraham <cjyabraham@gmail.com>
2024-12-03 10:38:54 -0800

    Add Golden Kubestronaut capability
    Signed-off-by: Chris Abraham <cjyabraham@gmail.com>

--- web/wp-content/mu-plugins/wp-mu-plugins/lf-mu/admin/partials/cpts.php
+++ web/wp-content/mu-plugins/wp-mu-plugins/lf-mu/admin/partials/cpts.php
@@ -180,7 +180,7 @@ $opts = array(
 		'singular_name' => __( 'Person' ),
 		'all_items'     => __( 'All People' ),
 	),
-	'public'              => false,
+	'public'              => true,
 	'has_archive'         => false,
 	'show_in_nav_menus'   => false,
 	'show_in_rest'        => true,
