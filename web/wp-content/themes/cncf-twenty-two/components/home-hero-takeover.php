d5a3791cd42e636d925fee89b6298bedd03ca712
Chris Abraham <cjyabraham@gmail.com>
2025-01-27 08:33:23 -0500

    Launch home hero takeover for Kubecon EU 2025
    Signed-off-by: Chris Abraham <cjyabraham@gmail.com>

--- web/wp-content/themes/cncf-twenty-two/components/home-hero-takeover.php
+++ web/wp-content/themes/cncf-twenty-two/components/home-hero-takeover.php
@@ -51,19 +51,33 @@ $metrics = LF_Utils::get_homepage_metrics();
 		<div class="container wrap">
 
 			<h1 class="home-hero__title">JOIN US IN<br />
-				DELHI, INDIA<br />
-				<span>DECEMBER 11-12</span>
+				LONDON, ENGLAND<br />
+				<span>1 - 4 April</span>
 			</h1>
 
 			<h2 class="home-hero__description"><strong>KubeCon + CloudNativeCon</strong> is the flagship conference for cloud native.</h2>
 			<div style="height:20px;" aria-hidden="true" class="wp-block-spacer"></div>
 
-			<div class="wp-block-buttons is-content-justification-left is-layout-flex wp-container-core-buttons-is-layout-2 wp-block-buttons-is-layout-flex">
-				<div class="wp-block-button show-over-600"><a class="wp-block-button__link has-text-align-center wp-element-button" href="https://events.linuxfoundation.org/kubecon-cloudnativecon-india/?utm_source=www&utm_medium=homepage&utm_campaign=KubeCon-india-2024&utm_content=hero" title="Learn more about KubeCon + CloudNativeCon">Learn More</a></div>
-				<div class="wp-block-button"><a class="wp-block-button__link has-text-align-center wp-element-button" href="https://events.linuxfoundation.org/kubecon-cloudnativecon-india/register/?utm_source=www&utm_medium=homepage&utm_campaign=KubeCon-india-2024&utm_content=hero" title="Register for KubeCon + CloudNativeCon">Register</a></div>
-			</div>
+			<div class="horizontal-rule"></div>
 
-			<div style="height:140px;" aria-hidden="true" class="wp-block-spacer"></div>
+			<ul class="home-hero__metric_wrapper">
+				<li class="show-over-800">
+					<div class="wp-block-button"><a href="https://events.linuxfoundation.org/kubecon-cloudnativecon-europe/register/?utm_source=www&utm_medium=homepage&utm_campaign=KubeCon-EU-2025&utm_content=hero" class="wp-block-button__link wp-element-button" title="Buy Tickets">Buy Tickets</a></div>
+				</li>
+				<li>10K+
+					<span>Attendees</span>
+				</li>
+				<li>200+
+					<span>Sponsors</span>
+				</li>
+				<li>300+
+					<span>Sessions</span>
+				</li>
+				<li class="show-over-600">
+						∞
+					<span>Swag</span>
+				</li>
+			</ul>
 		</div>
 	</div>
 </section>
