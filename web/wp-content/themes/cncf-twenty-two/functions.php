377736d3c13579ce27b5ebb17703a2c513007a3b
Chris Abraham <cjyabraham@gmail.com>
2025-09-05 12:54:26 +0700

    add scripts to "do not defer" list
    Signed-off-by: Chris Abraham <cjyabraham@gmail.com>

--- web/wp-content/themes/cncf-twenty-two/functions.php
+++ web/wp-content/themes/cncf-twenty-two/functions.php
@@ -154,8 +154,8 @@ function lf_defer_parsing_of_js( $url ) {
 		return $url;
 	}
 	// List of scripts that should not be deferred.
-	$do_not_defer_scripts = array( 'jquery' );
-	if ( ! is_front_page() && count( $do_not_defer_scripts ) > 0 ) {
+	$do_not_defer_scripts = array( 'jquery', 'osano-js', 'lfx-segment' );
+	if ( count( $do_not_defer_scripts ) > 0 ) {
 		foreach ( $do_not_defer_scripts as $script ) {
 			if ( strpos( $url, $script ) ) {
 				return $url;
