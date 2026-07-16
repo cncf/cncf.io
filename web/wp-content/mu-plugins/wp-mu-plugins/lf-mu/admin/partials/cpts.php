5c5e682c5da6d4621076a16b4c182190e83d20d0
Chris Abraham <cjyabraham@gmail.com>
2026-07-16 08:09:35 -0700

    re-hide projects
    Signed-off-by: Chris Abraham <cjyabraham@gmail.com>

--- web/wp-content/mu-plugins/wp-mu-plugins/lf-mu/admin/partials/cpts.php
+++ web/wp-content/mu-plugins/wp-mu-plugins/lf-mu/admin/partials/cpts.php
@@ -25,7 +25,7 @@ $opts = array(
 	'has_archive'       => false,
 	'show_in_nav_menus' => false,
 	'show_in_rest'      => true,
-	'show_ui'           => true,
+	'show_ui'           => false,
 	'hierarchical'      => false,
 	'rewrite'           => array( 'slug' => 'projects' ),
 	'supports'          => array( 'title', 'editor', 'thumbnail', 'revisions', 'custom-fields' ),
